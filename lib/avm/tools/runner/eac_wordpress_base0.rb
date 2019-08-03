66e166f2a72582d6b11d43e1d86bba34cf1e8831
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-03 11:12:17 -0300

    Avm::Tools::Runner::EacWordpressBase0.

---
+++ lib/avm/tools/runner/eac_wordpress_base0.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/simple_cache'
+require 'avm/stereotypes/eac_wordpress_base0/instance'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class EacWordpressBase0 < ::EacRubyUtils::Console::DocoptRunner
+        include ::EacRubyUtils::SimpleCache
+
+        DOC = <<~DOCOPT
+          Utilities for EacWordpressBase0 instances.
+
+          Usage:
+            __PROGRAM__ [options] <instance_id> __SUBCOMMANDS__
+            __PROGRAM__ -h | --help
+
+          Options:
+            -h --help             Show this screen.
+        DOCOPT
+
+        private
+
+        def instance_uncached
+          ::Avm::Stereotypes::EacWordpressBase0::Instance.by_id(options['<instance_id>'])
+        end
+      end
+    end
+  end
+end
