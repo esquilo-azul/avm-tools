dd47852e4e258d0376198e2035af0ad416733d3b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-20 14:46:19 -0300

    Avm::Tools::Runner::Files::Rotate: shows message and exit if error occurs.

--- lib/avm/tools/runner/files/rotate.rb
+++ lib/avm/tools/runner/files/rotate.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/console/speaker'
 require 'avm/files/rotate'
 
 module Avm
@@ -8,6 +9,8 @@ module Avm
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class Files < ::EacRubyUtils::Console::DocoptRunner
         class Rotate < ::EacRubyUtils::Console::DocoptRunner
+          include ::EacRubyUtils::Console::Speaker
+
           DOC = <<~DOCOPT
             Rotates a file (Like a backup).
 
@@ -20,7 +23,8 @@ module Avm
           DOCOPT
 
           def run
-            ::Avm::Files::Rotate.new(options.fetch('<path>')).run
+            error_message = ::Avm::Files::Rotate.new(options.fetch('<path>')).run
+            fatal_error(error_message) if error_message
           end
         end
       end
