f030a4b4666521e8dce06a4db76679db20bdea85
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-22 15:49:43 -0300

    Avm::Tools::Runner: option "--version".

--- lib/avm/tools/runner.rb
+++ lib/avm/tools/runner.rb
@@ -1,22 +1,36 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/console/speaker'
 require 'eac_ruby_utils/require_sub'
 ::EacRubyUtils.require_sub(__FILE__)
+require 'avm/tools/version'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
+      include ::EacRubyUtils::Console::Speaker
+
       DOC = <<~DOCOPT
         Tools for AVM.
 
         Usage:
           __PROGRAM__ [options] __SUBCOMMANDS__
+          __PROGRAM__ --version
           __PROGRAM__ -h | --help
 
         Options:
           -h --help             Show this screen.
+          -V --version          Show version.
       DOCOPT
+
+      def run
+        if options.fetch('--version')
+          out(::Avm::Tools::VERSION + "\n")
+        else
+          run_with_subcommand
+        end
+      end
     end
   end
 end
