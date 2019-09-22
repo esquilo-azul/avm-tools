278bb1e445331b0c95f84afb5a4c1031ce384176
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-22 17:54:52 -0300

    Avm::Tools::Runner::EacWordpressBase0::Data::Dump: refact.

--- lib/avm/tools/runner/eac_wordpress_base0/data/dump.rb
+++ lib/avm/tools/runner/eac_wordpress_base0/data/dump.rb
@@ -3,6 +3,7 @@
 require 'active_support/core_ext/numeric/time'
 require 'eac_ruby_utils/console/speaker'
 require 'eac_ruby_utils/simple_cache'
+require 'avm/data/package/dump'
 
 module Avm
   module Tools
@@ -31,16 +32,20 @@ module Avm
             DOCUMENT
 
             def run
-              if run_dump?
-                run_dump
+              if package_dump.runnable?
+                package_dump.run
               else
-                warn(format(NO_DUMP_MESSAGE, dump_path))
+                warn(package_dump.cannot_run_reason)
               end
               success("Dump path: \"#{dump_path}\"")
               dump_path
             end
 
             private
+
+            def package_dump_uncached
+              context(:instance).data_package.dump(dump_path, existing: package_dump_existing)
+            end
 
             def dump_path
               options.fetch('--dump-path') || default_dump_path
@@ -50,58 +55,12 @@ module Avm
               context(:instance).read_entry(DEFAULT_DUMP_PATH_ENTRY_SUFFIX)
             end
 
-            def dump_exist?
-              ::File.exist?(dump_path)
-            end
-
-            def dump_time_uncached
-              dump_exist? ? ::Time.now - ::File.mtime(dump_path) : nil
-            end
-
-            def dump_expired?
-              return false unless dump_time.present?
-
-              dump_time >= DUMP_EXPIRE_TIME
-            end
-
-            def run_dump
-              download_dump
-              rotate_dump
-              move_download_to_final_dest
-            end
-
-            def download_dump
-              infom 'Downloading dump...'
-              dump_command.system!(output_file: download_path)
-              fatal_error "File \"#{download_path}\" not saved" unless ::File.exist?(download_path)
-              fatal_error "File \"#{download_path}\" is empty" if ::File.zero?(download_path)
-            end
-
-            def move_download_to_final_dest
-              ::FileUtils.mv(download_path, dump_path)
-            end
-
-            def rotate_dump
-              return unless dump_exist?
-
-              info "Rotating \"#{dump_path}\"..."
-              ::Avm::Files::Rotate.new(dump_path).run
-            end
-
-            def download_path_uncached
-              f = ::Tempfile.new('eac_rails_base0_data_dump')
-              path = f.path
-              f.close
-              f.unlink
-              path
-            end
-
-            def run_dump?
-              !dump_exist? || options.fetch('--rewrite') || dump_expired?
-            end
-
-            def dump_command
-              context(:instance).pg.dump_gzip_command
+            def package_dump_existing
+              if options.fetch('--rewrite')
+                ::Avm::Data::Package::Dump::EXISTING_ROTATE
+              else
+                ::Avm::Data::Package::Dump::EXISTING_ROTATE_EXPIRED
+              end
             end
           end
         end
