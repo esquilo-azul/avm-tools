e24b85a6b0cb3021ed3e874ac91a5be0d16bbed0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-22 17:51:12 -0300

    Avm::Stereotypes::EacWordpressBase0::Instance.data_package.

--- lib/avm/stereotypes/eac_wordpress_base0/instance.rb
+++ lib/avm/stereotypes/eac_wordpress_base0/instance.rb
@@ -2,6 +2,8 @@
 
 require 'avm/instances/base'
 require 'avm/stereotypes/postgresql/instance_with'
+require 'avm/data/instance/package'
+require 'avm/stereotypes/eac_wordpress_base0/uploads_data_unit'
 
 module Avm
   module Stereotypes
@@ -16,6 +18,24 @@ module Avm
         def run_subcommand(subcommand_class, argv)
           parent = ::OpenStruct.new(instance: self)
           subcommand_class.new(argv: argv, parent: parent).run
+        end
+
+        def data_package
+          @data_package ||= ::Avm::Data::Instance::Package.new(
+            self, units: { database: database_unit, uploads: UploadsDataUnit.new(self) }
+          )
+        end
+
+        def database_unit
+          self_instance = self
+          pg.data_unit.after_load do
+            info 'Fixing web addresses...'
+            run_sql(<<~SQL)
+              update wp_options
+              set option_value = '#{self_instance.read_entry('url')}'
+              where option_name in ('siteurl', 'home')
+            SQL
+          end
         end
       end
     end
