36451865a8ef0a26219eb8645403452a2adaac9e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-09-24 16:34:37 -0300

    Avm::Runner::EacWordpressBase0::Data::Load: refact.

--- lib/avm/tools/runner/eac_wordpress_base0/data/load.rb
+++ lib/avm/tools/runner/eac_wordpress_base0/data/load.rb
@@ -29,9 +29,7 @@ module Avm
               return ::Dev::Result.error("Dump \"#{dump_path}\" does not exist") unless
               ::File.exist?(dump_path)
 
-              before_load
               load_dump
-              fix_web_addresses
               success("Dump loaded from \"#{dump_path}\"")
             end
 
@@ -50,39 +48,18 @@ module Avm
 
             def load_dump
               info "Loading dump \"#{dump_path}\"..."
-              ::EacRubyUtils::Envs.local.command('cat', dump_path).pipe(load_command).execute!
+              package_load.run
+              #::EacRubyUtils::Envs.local.command('cat', dump_path).pipe(load_command).execute!
             end
 
             def dump_instance_method
               :dump_database
             end
 
-            def before_load
-              info 'Clearing database (Dropping all tables)...'
-              sql = run_sql(drop_all_tables_sql)
-              run_sql(sql) if sql.present?
-            end
+            private
 
-            def drop_all_tables_sql
-              "select 'drop table \"' || tablename || '\" cascade;' from pg_tables " \
-                "where schemaname = 'public';"
-            end
-
-            def run_sql(sql)
-              context(:instance).pg.psql_command_command(sql).execute!
-            end
-
-            def load_command
-              context(:instance).pg.psql_command.prepend(['gzip', '-d', '@ESC_|'])
-            end
-
-            def fix_web_addresses
-              info 'Fixing web addresses...'
-              run_sql(<<~SQL)
-                update wp_options
-                set option_value = '#{context(:instance).read_entry('url')}'
-                where option_name in ('siteurl', 'home')
-              SQL
+            def package_load_uncached
+              context(:instance).data_package.load(dump_path)
             end
           end
         end
