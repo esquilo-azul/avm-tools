07f0855bb1a3ccb629ffaf25b9e7065c645451e4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-04 14:36:31 -0300

    Object.template_path.

--- lib/avm/patches/object/template.rb
+++ lib/avm/patches/object/template.rb
@@ -8,9 +8,17 @@ class Object
     def template
       @template ||= ::Avm::Templates.template(name.underscore)
     end
+
+    def template_path
+      @template_path ||= ::Avm::Templates.template_path(name.underscore)
+    end
   end
 
   def template
     self.class.template
+  end
+
+  def template_path
+    self.class.template_path
   end
 end
