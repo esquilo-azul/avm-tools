5e0586c78c73477c5890dd634146af880b467f5a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-03 23:30:17 -0300

    Avm::Git::Commit: appendable directories.

--- lib/avm/git/commit/deploy.rb
+++ lib/avm/git/commit/deploy.rb
@@ -4,6 +4,7 @@ require 'addressable'
 require 'eac_ruby_utils/simple_cache'
 require 'eac_ruby_utils/require_sub'
 ::EacRubyUtils.require_sub(__FILE__)
+require 'avm/templates/directory'
 
 module Avm
   module Git
@@ -37,21 +38,60 @@ module Avm
       class Deploy
         include ::EacRubyUtils::SimpleCache
 
-        attr_reader :commit, :target_env, :target_path
+        attr_reader :commit, :target_env, :target_path, :appended_directories, :variables_source
 
         def initialize(commit, target_env, target_path)
           @commit = commit
           @target_env = target_env
           @target_path = target_path
-          run
+          @appended_directories = []
+          @variables_source = nil
+        end
+
+        def append_directory(directory)
+          @appended_directories << directory
+          self
+        end
+
+        def append_directories(directories)
+          directories.each { |directory| append_directory(directory) }
+          self
+        end
+
+        def variables_source_set(source)
+          @variables_source = source
+          self
+        end
+
+        def run
+          on_build_dir do
+            copy_git_content
+            appended_directories.each { |directory| copy_appended_directory(directory) }
+            mkdir_target
+            clear_content
+            send_untar_package
+          end
         end
 
         private
 
-        def run
-          mkdir_target
-          clear_content
-          send_untar_package
+        attr_reader :build_dir
+
+        def on_build_dir
+          @build_dir = ::Dir.mktmpdir
+          yield
+        ensure
+          ::FileUtils.rm_rf(@build_dir)
+        end
+
+        def copy_git_content
+          git_archive_command.pipe(untar_git_archive_command).execute!
+        end
+
+        def copy_appended_directory(directory)
+          raise 'Variables source not set' if variables_source.blank?
+
+          ::Avm::Templates::Directory.new(directory).apply(variables_source, build_dir)
         end
 
         def mkdir_target
@@ -65,15 +105,27 @@ module Avm
         end
 
         def send_untar_package
-          git_archive_command.pipe(untar_command).execute!
+          tar_build_command.pipe(untar_build_command).execute!
         end
 
         def git_archive_command
           commit.git.command('archive', '--format=tar', commit.sha1)
         end
 
-        def untar_command
-          target_env.command('tar', '-xf', '-', '-C', target_path)
+        def untar_git_archive_command
+          source_env.command('tar', '-xf', '-', '-C', build_dir)
+        end
+
+        def tar_build_command
+          source_env.command('tar', '-czO', '-C', build_dir, '.')
+        end
+
+        def untar_build_command
+          target_env.command('tar', '-xzf', '-', '-C', target_path)
+        end
+
+        def source_env
+          ::EacRubyUtils::Envs.local
         end
       end
     end
