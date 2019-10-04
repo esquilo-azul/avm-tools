2b8a52622357bcf2f91057e02edf9f019ca75be3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-04 11:53:11 -0300

    Supress EacRubyUtils::Console::Speaker input/output for RSpec.

---
+++ spec/spec_helper/console_speaker.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/speaker'
+
+class FailIfRequestInput
+  %w[gets noecho].each do |method|
+    define_method(method) do
+      raise "Input method requested: #{method}. Should not request input on RSpec."
+    end
+  end
+end
+
+::EacRubyUtils::Console::Speaker.current_node.configure do |c|
+  c.stderr = ::StringIO.new
+  c.stdout = ::StringIO.new
+  c.stdin = ::FailIfRequestInput.new
+end
