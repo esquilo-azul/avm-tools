e2bc59a3b501522de734e6abc423f97273ce65e9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-08 22:02:51 -0300

    EacLauncher::Git::Base.dirty_files.

--- lib/avm/patches/eac_launcher_git_base.rb
+++ lib/avm/patches/eac_launcher_git_base.rb
@@ -18,6 +18,21 @@ module Avm
         args, _options = build_args(args)
         ::EacRubyUtils::Envs.local.command(*args)
       end
+
+      def dirty_files
+        execute!('status', '--porcelain', '--untracked-files').each_line.map do |line|
+          parse_status_line(line.gsub(/\n\z/, ''))
+        end
+      end
+
+      private
+
+      def parse_status_line(line)
+        m = /\A(.)(.)\s(.+)\z/.match(line)
+        ::Kernel.raise "Status pattern does not match \"#{line}\"" unless m
+        ::OpenStruct.new(index: m[1], worktree: m[2], path: m[3],
+                         absolute_path: ::File.expand_path(m[3], self))
+      end
     end
   end
 end
