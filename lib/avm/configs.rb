b978f885faef68a754b11ef0a05ff5f67458d94b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-02 20:24:39 -0300

    Avm.configs_storage_path.

--- lib/avm/configs.rb
+++ lib/avm/configs.rb
@@ -4,8 +4,19 @@ require 'eac_ruby_utils/console/configs'
 
 module Avm
   class << self
+    attr_reader :configs_storage_path
+
     def configs
-      @configs ||= ::EacRubyUtils::Console::Configs.new('avm-tools')
+      @configs ||= ::EacRubyUtils::Console::Configs.new('avm-tools', configs_options)
+    end
+
+    def configs_options
+      configs_storage_path.if_present({}) { |v| { storage_path: v } }
+    end
+
+    def configs_storage_path=(path)
+      @configs_storage_path = path
+      @configs = nil
     end
   end
 end
