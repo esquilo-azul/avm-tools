048e6938596abc66f8c3619668f2d1ae83b88a14
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-02 14:54:11 -0300

    Avm::Stereotypes::EacWebappBase0.

---
+++ lib/avm/stereotypes/eac_webapp_base0.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/require_sub'
+::EacRubyUtils.require_sub(__FILE__)
+
+module Avm
+  module Stereotypes
+    module EacWebappBase0
+    end
+  end
+end
