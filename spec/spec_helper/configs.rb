13ecc3c911f0833ac0a4fd1d81fb5c60bdedafce
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-02 21:01:15 -0300

    RSpec: shows entry if console input is requested.

--- spec/spec_helper/configs.rb
+++ spec/spec_helper/configs.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-require 'avm/configs'
+require 'eac_ruby_utils/console/configs'
 require 'tempfile'
+require 'avm/configs'
 
 temp_file = ::Tempfile.new('avm-tools_rspec_storage')
 temp_storage_path = temp_file.path
@@ -14,5 +15,17 @@ RSpec.configure do |config|
 
   config.after do
     ::File.unlink(temp_storage_path) if ::File.exist?(temp_storage_path)
+  end
+end
+
+module EacRubyUtils
+  module Console
+    class Configs
+      protected
+
+      def read_entry_from_console(entry_key, _options)
+        raise "Console input requested for entry \"#{entry_key}\""
+      end
+    end
   end
 end
