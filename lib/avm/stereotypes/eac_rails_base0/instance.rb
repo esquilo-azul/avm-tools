4e94123d1ce23ab54caad7c2272983a415b53e51
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-05 11:21:18 -0300

    Avm::Stereotypes::EacRailsBase0::Instance.bundle.

--- lib/avm/stereotypes/eac_rails_base0/instance.rb
+++ lib/avm/stereotypes/eac_rails_base0/instance.rb
@@ -7,6 +7,13 @@ module Avm
     module EacRailsBase0
       class Instance < ::Avm::Stereotypes::EacWebappBase0::Instance
         FILES_UNITS = { uploads: 'public/uploads' }.freeze
+
+        def bundle(*args)
+          host_env.command('bundle', *args)
+                  .envvar('BUNDLE_GEMFILE', ::File.join(read_entry('fs_path'), 'Gemfile'))
+                  .envvar('RAILS_ENV', 'production')
+                  .chdir(read_entry('fs_path'))
+        end
       end
     end
   end
