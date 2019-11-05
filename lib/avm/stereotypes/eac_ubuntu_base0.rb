b338abf93ee124d35091c8ce9113516537d605ae
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-05 11:01:46 -0300

    Avm::Stereotypes::EacUbuntuBase0.

---
+++ lib/avm/stereotypes/eac_ubuntu_base0.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/require_sub'
+::EacRubyUtils.require_sub(__FILE__)
+
+module Avm
+  module Stereotypes
+    module EacUbuntuBase0
+    end
+  end
+end
