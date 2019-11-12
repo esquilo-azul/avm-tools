40f297a9c1eaa4e90551c4e3f37eed260b75a53e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-12 13:45:02 -0300

    "RSpec.describe Avm::Instances::Base.by_id".

--- spec/lib/avm/instances/base_spec.rb
+++ spec/lib/avm/instances/base_spec.rb
@@ -47,4 +47,23 @@ RSpec.describe ::Avm::Instances::Base do
       end
     end
   end
+
+  describe '#by_id' do
+    {
+      'avm-tools_0' => %w[avm-tools 0],
+      'avm-tools_dev' => %w[avm-tools dev]
+    }.each do |id, expected|
+      context "when input ID is \"#{id}\"" do
+        let(:instance) { described_class.by_id(id) }
+
+        it "returns application.id=#{expected.first}" do
+          expect(instance.application.id).to eq(expected.first)
+        end
+
+        it "returns instance.suffix=#{expected.last}" do
+          expect(instance.suffix).to eq(expected.last)
+        end
+      end
+    end
+  end
 end
