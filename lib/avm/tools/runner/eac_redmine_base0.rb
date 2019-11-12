8ef26d29c1c8a2133c046705fed51a38b874c9b0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-12 13:03:32 -0300

    Avm::Tools::Runner::EacRedmineBase0.

---
+++ lib/avm/tools/runner/eac_redmine_base0.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/simple_cache'
+require 'avm/stereotypes/eac_redmine_base0/instance'
+require 'eac_ruby_utils/require_sub'
+::EacRubyUtils.require_sub(__FILE__)
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class EacRedmineBase0 < ::EacRubyUtils::Console::DocoptRunner
+        include ::EacRubyUtils::SimpleCache
+
+        DOC = <<~DOCOPT
+          Utilities for EacRedmineBase0 instances.
+
+          Usage:
+            __PROGRAM__ [options] <instance_id> __SUBCOMMANDS__
+            __PROGRAM__ -h | --help
+
+          Options:
+            -h --help             Show this screen.
+        DOCOPT
+
+        private
+
+        def instance_uncached
+          ::Avm::Stereotypes::EacRedmineBase0::Instance.by_id(options['<instance_id>'])
+        end
+      end
+    end
+  end
+end
