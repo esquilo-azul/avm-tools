e68f09b1b549f092b7720d018994d30151568459
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-12 12:45:31 -0300

    Avm: refact.

--- lib/avm.rb
+++ lib/avm.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/require_sub'
+
 module Avm
-  require 'avm/configs'
-  require 'avm/data'
-  require 'avm/executables'
-  require 'avm/git'
-  require 'avm/instances'
-  require 'avm/patches'
-  require 'avm/result'
-  require 'avm/stereotypes'
-  require 'avm/templates'
-  require 'avm/tools'
+  ::EacRubyUtils.require_sub(__FILE__)
 end
