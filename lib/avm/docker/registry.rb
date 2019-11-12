9c0a6de991675d6c79c5a7716cfa8c7602ab54a5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-12 14:04:32 -0300

    Avm::Docker::Registry.

---
+++ lib/avm/docker/registry.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Docker
+    class Registry
+      class << self
+        def default
+          @default ||= new(::Avm.configs.read_entry('docker.registry.name'))
+        end
+      end
+
+      common_constructor :name
+
+      def to_s
+        name
+      end
+
+      def sub(suffix)
+        self.class.new("#{name}#{suffix}")
+      end
+    end
+  end
+end
