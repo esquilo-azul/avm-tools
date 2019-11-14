9ea5eb0a1d215308587477abe7acf510f270fc61
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-14 15:42:35 -0300

    Avm::Tools::Runner::Self.

---
+++ lib/avm/tools/runner/self.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/require_sub'
+require 'avm/self'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class Self < ::EacRubyUtils::Console::DocoptRunner
+        ::EacRubyUtils.require_sub(__FILE__)
+
+        DOC = <<~DOCOPT
+          Utilities for self avm-tools.
+
+          Usage:
+            __PROGRAM__ [options] __SUBCOMMANDS__
+            __PROGRAM__ -h | --help
+
+          Options:
+            -h --help             Show this screen.
+        DOCOPT
+
+        def instance
+          ::Avm::Self.instance
+        end
+      end
+    end
+  end
+end
