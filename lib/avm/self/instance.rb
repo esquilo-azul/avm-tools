a603d1fd20ace0c6ead31fcc91fd816cd24d88d4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-19 19:09:22 -0300

    Avm::Self::Instance.

---
+++ lib/avm/self/instance.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'avm/instances/base'
+require 'avm/self/docker_image'
+require 'avm/stereotypes/eac_ubuntu_base0/docker_image'
+
+module Avm
+  module Self
+    class Instance < ::Avm::Instances::Base
+      def docker_image_class
+        ::Avm::Self::DockerImage
+      end
+
+      def docker_run_arguments
+        ['-e', "LOCAL_USER_ID=#{::Process.uid}"]
+      end
+    end
+  end
+end
