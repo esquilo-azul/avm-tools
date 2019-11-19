f88e1eacc38638a5f667bacb795701be78eecef9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-11-19 19:09:09 -0300

    Avm::Self.instance.

--- lib/avm/self.rb
+++ lib/avm/self.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/require_sub'
+require 'avm/instances/base'
 
 module Avm
   module Self
     ::EacRubyUtils.require_sub(__FILE__)
+
+    class << self
+      def instance
+        @instance ||= ::Avm::Self::Instance.by_id('avm-tools_self')
+      end
+    end
   end
 end
