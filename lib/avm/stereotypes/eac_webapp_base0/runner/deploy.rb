3e723fde3595de383db139ee18ee81e06a0cb464
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-12-20 22:28:17 -0300

    Avm::Stereotypes::EacWebappBase0::Runner::Deploy: generalize Avm::Tools::Runner::EacRailsBase0::Deploy.

---
+++ lib/avm/stereotypes/eac_webapp_base0/runner/deploy.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/console/speaker'
+
+module Avm
+  module Stereotypes
+    module EacWebappBase0
+      module Runner
+        class Deploy < ::EacRubyUtils::Console::DocoptRunner
+          include ::EacRubyUtils::Console::Speaker
+
+          DOC = <<~DOCOPT
+            Deploy for %%STEREOTYPE_NAME%% instance.
+
+            Usage:
+              __PROGRAM__ [options]
+              __PROGRAM__ -h | --help
+
+            Options:
+              -h --help                       Show this screen.
+              -r --reference=<git-reference>  Git reference to deploy.
+          DOCOPT
+          def initialize(settings = {})
+            super settings.merge(doc: DOC.gsub('%%STEREOTYPE_NAME%%', stereotype_name))
+          end
+
+          def deploy_class
+            stereotype_module.const_get('Deploy')
+          end
+
+          def stereotype_module
+            "Avm::Stereotypes::#{stereotype_name}".constantize
+          end
+
+          def stereotype_name
+            self.class.name.deconstantize.demodulize
+          end
+
+          def run
+            result = deploy_class.new(context(:instance), options.fetch('--reference')).run
+            if result.error?
+              fatal_error result.to_s
+            else
+              infov 'Result', result.label
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- lib/avm/tools/runner/eac_rails_base0/deploy.rb
+++ lib/avm/tools/runner/eac_rails_base0/deploy.rb
@@ -1,39 +1,13 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/console/speaker'
 require 'avm/stereotypes/eac_rails_base0/deploy'
+require 'avm/stereotypes/eac_webapp_base0/runner/deploy'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class EacRailsBase0 < ::EacRubyUtils::Console::DocoptRunner
-        class Deploy < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacRubyUtils::Console::Speaker
-
-          DOC = <<~DOCOPT
-            Deploy for EacRailsBase0 instance.
-
-            Usage:
-              __PROGRAM__ [options]
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                       Show this screen.
-              -r --reference=<git-reference>  Git reference to deploy.
-          DOCOPT
-
-          def run
-            result = ::Avm::Stereotypes::EacRailsBase0::Deploy.new(
-              context(:instance),
-              options.fetch('--reference')
-            ).run
-            if result.error?
-              fatal_error result.to_s
-            else
-              infov 'Result', result.label
-            end
-          end
+        class Deploy < ::Avm::Stereotypes::EacWebappBase0::Runner::Deploy
         end
       end
     end
