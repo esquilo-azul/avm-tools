41ccce955127bbf870b263c50f059c66c81ae1f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-12-20 22:30:20 -0300

    Avm::Tools::Runner::EacWordpressBase0::Deploy: use Avm::Stereotypes::EacWebappBase0::Runner::Deploy.

--- lib/avm/tools/runner/eac_wordpress_base0/deploy.rb
+++ lib/avm/tools/runner/eac_wordpress_base0/deploy.rb
@@ -1,39 +1,13 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/console/speaker'
+require 'avm/stereotypes/eac_webapp_base0/runner/deploy'
 require 'avm/stereotypes/eac_wordpress_base0/deploy'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class EacWordpressBase0 < ::EacRubyUtils::Console::DocoptRunner
-        class Deploy < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacRubyUtils::Console::Speaker
-
-          DOC = <<~DOCOPT
-            Deploy for EacWordpressBase0 instance.
-
-            Usage:
-              __PROGRAM__ [options]
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                       Show this screen.
-              -r --reference=<git-reference>  Git reference to deploy.
-          DOCOPT
-
-          def run
-            result = ::Avm::Stereotypes::EacWordpressBase0::Deploy.new(
-              context(:instance),
-              options.fetch('--reference')
-            ).run
-            if result.error?
-              fatal_error result.to_s
-            else
-              infov 'Result', result.label
-            end
-          end
+        class Deploy < ::Avm::Stereotypes::EacWebappBase0::Runner::Deploy
         end
       end
     end
