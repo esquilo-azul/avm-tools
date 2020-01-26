17923a9f1193325b62414d0a2ce4f7e0b5e02501
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-01-26 18:14:37 -0300

    Avm::Stereotypes::EacWebappBase0::Runner::Data::Dump: extracted from Avm::Tools::Runner::EacWordpressBase0::Data::Dump.

--- lib/avm/tools/runner/eac_wordpress_base0/data/dump.rb
+++ lib/avm/tools/runner/eac_wordpress_base0/data/dump.rb
@@ -1,67 +1,14 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/numeric/time'
-require 'eac_ruby_utils/console/speaker'
-require 'eac_ruby_utils/simple_cache'
-require 'avm/data/package/dump'
+require 'eac_ruby_utils/console/docopt_runner'
+require 'avm/stereotypes/eac_webapp_base0/runner/data/dump'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class EacWordpressBase0 < ::EacRubyUtils::Console::DocoptRunner
         class Data < ::EacRubyUtils::Console::DocoptRunner
-          class Dump < ::EacRubyUtils::Console::DocoptRunner
-            include ::EacRubyUtils::SimpleCache
-            include ::EacRubyUtils::Console::Speaker
-
-            DUMP_EXPIRE_TIME = 1.days
-            DEFAULT_DUMP_PATH_ENTRY_SUFFIX = 'data.default_dump_path'
-            NO_DUMP_MESSAGE = 'Dump "%s" already exist and rewrite options was no setted nor ' \
-              'dump was expired.'
-
-            DOC = <<~DOCUMENT
-              Dump utility for EacRailsBase instance.
-
-                  Usage:
-                  __PROGRAM__ [options]
-
-                Options:
-                  -h --help               Show this screen.
-                  --rewrite               Forces dump overwrite.
-                  --dump-path=<dump_path> Set DUMP_PATH variable.
-            DOCUMENT
-
-            def run
-              if package_dump.runnable?
-                package_dump.run
-              else
-                warn(package_dump.cannot_run_reason)
-              end
-              success("Dump path: \"#{dump_path}\"")
-              dump_path
-            end
-
-            private
-
-            def package_dump_uncached
-              context(:instance).data_package.dump(dump_path, existing: package_dump_existing)
-            end
-
-            def dump_path
-              options.fetch('--dump-path') || default_dump_path
-            end
-
-            def default_dump_path
-              context(:instance).read_entry(DEFAULT_DUMP_PATH_ENTRY_SUFFIX)
-            end
-
-            def package_dump_existing
-              if options.fetch('--rewrite')
-                ::Avm::Data::Package::Dump::EXISTING_ROTATE
-              else
-                ::Avm::Data::Package::Dump::EXISTING_ROTATE_EXPIRED
-              end
-            end
+          class Dump < ::Avm::Stereotypes::EacWebappBase0::Runner::Data::Dump
           end
         end
       end
