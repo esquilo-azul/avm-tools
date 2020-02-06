4b34c5f47a955841dc2105c5ec566ddabce992b5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-02-06 17:18:04 -0300

    Avm::Tools::Runner::EacRedmineBase0::Data.

---
+++ lib/avm/tools/runner/eac_redmine_base0/data.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'avm/stereotypes/eac_webapp_base0/runner/data'
+require 'eac_ruby_utils/console/docopt_runner'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class EacRedmineBase0 < ::EacRubyUtils::Console::DocoptRunner
+        class Data < ::EacRubyUtils::Console::DocoptRunner
+          ::EacRubyUtils.require_sub(__FILE__)
+
+          DOC = <<~DOCOPT
+            Data utilities for EacRailsBase0 instances.
+
+            Usage:
+              __PROGRAM__ __SUBCOMMANDS__
+              __PROGRAM__ -h | --help
+
+            Options:
+              -h --help             Show this screen.
+          DOCOPT
+        end
+      end
+    end
+  end
+end
