e36b455d557f6ea1c9066666e3a552bf7e5cdef4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-02-06 18:06:05 -0300

    Avm::Stereotypes::EacRedmineBase0::DataUnit.

---
+++ lib/avm/stereotypes/eac_redmine_base0/data_unit.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'avm/data/instance/unit'
+require 'eac_ruby_utils/core_ext'
+require 'open-uri'
+
+module Avm
+  module Stereotypes
+    module EacRedmineBase0
+      class DataUnit < ::Avm::Data::Instance::Unit
+        common_constructor :instance
+
+        EXTENSION = '.tar'
+
+        def do_dump(data_path)
+          ::File.open(data_path, 'wb') do |file|
+            file << URI.parse(export_url).read
+          end
+        end
+
+        def export_url
+          uri = ::Addressable::URI.parse(instance.read_entry('web.url')) + '/backup/export'
+          uri.query_values = { key: instance.read_entry('admin.api_key') }
+          uri.to_s
+        end
+      end
+    end
+  end
+end
