47e70bfdfd6628d375f9364f25ad48a90ac8c43c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-15 12:15:41 -0300

    Avm::Tools::Runner::EacRedmineBase0::Runner.

---
+++ lib/avm/tools/runner/eac_redmine_base0/runner.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_cli/default_runner'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class EacRedmineBase0 < ::EacRubyUtils::Console::DocoptRunner
+        class Runner < ::EacRubyUtils::Console::DocoptRunner
+          include ::EacCli::DefaultRunner
+
+          runner_definition do
+            desc 'Runs a Ruby code with "rails runner".'
+            arg_opt '-e', '--environment', 'Specifies the environment for the runner to operate' \
+              " under (test/development/production).\n" \
+              'Default: development'
+            pos_arg 'code'
+          end
+
+          def run
+            infov 'Runner arguments', runner_args
+            infov 'Environment', context(:instance).host_env
+            runner_command.system!
+          end
+
+          def runner_command_uncached
+            context(:instance).bundle(*bundle_args)
+          end
+
+          def bundle_args
+            %w[exec rails runner] + runner_args
+          end
+
+          def runner_args
+            options.fetch('--environment').if_present([]) { |v| ['--environment', v] } +
+              [options.fetch('<code>')]
+          end
+        end
+      end
+    end
+  end
+end
