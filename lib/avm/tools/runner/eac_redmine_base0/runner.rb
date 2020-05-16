4e4c139cca64cb4340cc41c5ab01daa13b69d76d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-16 15:36:23 -0300

    Avm::Rails::Runners::Runner: move from Avm::Tools::Runner::EacRedmineBase0::Runner.

--- lib/avm/tools/runner/eac_redmine_base0/runner.rb
+++ lib/avm/tools/runner/eac_redmine_base0/runner.rb
@@ -1,41 +1,12 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_cli/default_runner'
+require 'avm/rails/runners/runner'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class EacRedmineBase0 < ::EacRubyUtils::Console::DocoptRunner
-        class Runner < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacCli::DefaultRunner
-
-          runner_definition do
-            desc 'Runs a Ruby code with "rails runner".'
-            arg_opt '-e', '--environment', 'Specifies the environment for the runner to operate' \
-              " under (test/development/production).\n" \
-              'Default: development'
-            pos_arg 'code'
-          end
-
-          def run
-            infov 'Runner arguments', runner_args
-            infov 'Environment', context(:instance).host_env
-            runner_command.system!
-          end
-
-          def runner_command_uncached
-            context(:instance).bundle(*bundle_args)
-          end
-
-          def bundle_args
-            %w[exec rails runner] + runner_args
-          end
-
-          def runner_args
-            options.fetch('--environment').if_present([]) { |v| ['--environment', v] } +
-              [options.fetch('<code>')]
-          end
+        class Runner < ::Avm::Rails::Runners::Runner
         end
       end
     end
