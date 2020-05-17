6b622ef553cc8efd1aa98279cf4fc95bc4beba45
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-17 15:50:14 -0300

    Avm::Rails::Runners::Bundle: generalize Avm::Tools::Runner::EacRedmineBase0::Bundle.

---
+++ lib/avm/rails/runners/bundle.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/console/docopt_runner'
+require 'shellwords'
+
+module Avm
+  module Rails
+    module Runners
+      class Bundle < ::EacRubyUtils::Console::DocoptRunner
+        enable_console_speaker
+
+        DOC = <<~DOCOPT
+          Runs "bundle ...".
+
+          Usage:
+            __PROGRAM__ [<bundle-args>...]
+            __PROGRAM__ -h | --help
+
+          Options:
+            -h --help                 Show this screen.
+        DOCOPT
+
+        def run
+          infov 'Bundle arguments', ::Shellwords.join(bundle_args)
+          context(:instance).bundle(*bundle_args).system!
+        end
+
+        def bundle_args
+          options.fetch('<bundle-args>').reject { |arg| arg == '--' }
+        end
+      end
+    end
+  end
+end
--- lib/avm/tools/runner/eac_redmine_base0/bundle.rb
+++ lib/avm/tools/runner/eac_redmine_base0/bundle.rb
@@ -1,34 +1,12 @@
 # frozen_string_literal: true
 
-require 'avm/stereotypes/eac_redmine_base0/deploy'
-require 'avm/stereotypes/eac_webapp_base0/runner/deploy'
+require 'avm/rails/runners/bundle'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class EacRedmineBase0 < ::EacRubyUtils::Console::DocoptRunner
-        class Bundle < ::EacRubyUtils::Console::DocoptRunner
-          enable_console_speaker
-
-          DOC = <<~DOCOPT
-            Runs "bundle ...".
-
-            Usage:
-              __PROGRAM__ [<bundle-args>...]
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                 Show this screen.
-          DOCOPT
-
-          def run
-            infov 'Bundle arguments', ::Shellwords.join(bundle_args)
-            context(:instance).bundle(*bundle_args).system!
-          end
-
-          def bundle_args
-            options.fetch('<bundle-args>').reject { |arg| arg == '--' }
-          end
+        class Bundle < ::Avm::Rails::Runners::Bundle
         end
       end
     end
