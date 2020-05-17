a8ba9999fd5585a01618fd1bc557f3e0abcf0ab4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-17 15:59:40 -0300

    Avm::Tools::Runner::EacRailsBase0::Bundle.

---
+++ lib/avm/tools/runner/eac_rails_base0/bundle.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'avm/rails/runners/bundle'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class EacRailsBase0 < ::EacRubyUtils::Console::DocoptRunner
+        class Bundle < ::Avm::Rails::Runners::Bundle
+        end
+      end
+    end
+  end
+end
