6989101189f2d1bbc5d57083e1f9b6b5d1097015
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-06-02 00:36:09 -0300

    Avm::Git::Issue::Complete.test_result: log test's result.

--- lib/avm/git/issue/complete/_test.rb
+++ lib/avm/git/issue/complete/_test.rb
@@ -2,6 +2,7 @@
 
 require 'avm/instances/configuration'
 require 'avm/result'
+require 'eac_ruby_utils/fs/temp'
 
 module Avm
   module Git
@@ -11,7 +12,10 @@ module Avm
           test_command = configuration.if_present(&:test_command)
           return ::Avm::Result.success('unconfigured') unless test_command.present?
 
-          if test_command.execute[:exit_code].zero?
+          infom "Running test command \"#{test_command}\"..."
+          result = test_command.execute
+          test_result_log(result)
+          if result.fetch(:exit_code).zero?
             ::Avm::Result.success('yes')
           else
             ::Avm::Result.error('no')
@@ -19,6 +23,16 @@ module Avm
         end
 
         private
+
+        def test_result_log(result)
+          stdout_file = ::EacRubyUtils::Fs::Temp.file
+          stderr_file = ::EacRubyUtils::Fs::Temp.file
+          stdout_file.write(result.fetch(:stdout))
+          stderr_file.write(result.fetch(:stderr))
+          infov '  * Exit code', result.fetch(:exit_code)
+          infov '  * STDOUT', stdout_file.to_path
+          infov '  * STDERR', stderr_file.to_path
+        end
 
         def configuration_uncached
           ::Avm::Instances::Configuration.find_by_path(@git)
