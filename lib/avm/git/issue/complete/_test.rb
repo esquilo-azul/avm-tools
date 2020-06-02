4ed4a3d6a15ae52e062d2595f2116e433f691b56
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-06-01 23:45:51 -0300

    Avm::Git::Issue::Complete.test_result: use Avm::Instances::Configuration.any_test_command.

--- lib/avm/git/issue/complete/_test.rb
+++ lib/avm/git/issue/complete/_test.rb
@@ -9,7 +9,7 @@ module Avm
     module Issue
       class Complete
         def test_result
-          test_command = configuration.if_present(&:test_command)
+          test_command = configuration.if_present(&:any_test_command)
           return ::Avm::Result.success('unconfigured') unless test_command.present?
 
           infom "Running test command \"#{test_command}\"..."
