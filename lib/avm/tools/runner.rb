f99b27e1962d8af671bbc9432c9e2e3f263d7ff4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-06 19:07:32 -0300

    Avm::Tools::Runner: use EacRubyBase0::Runner.

--- lib/avm/tools/runner.rb
+++ lib/avm/tools/runner.rb
@@ -1,39 +1,23 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/console/speaker'
 require 'eac_ruby_utils/require_sub'
 ::EacRubyUtils.require_sub(__FILE__)
+require 'avm/self'
 require 'avm/tools/version'
+require 'eac_ruby_base0/runner'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
-      include ::EacRubyUtils::Console::Speaker
+      include ::EacRubyBase0::Runner
 
-      DOC = <<~DOCOPT
-        Tools for AVM.
+      runner_definition do
+        desc 'Tools for AVM.'
+      end
 
-        Usage:
-          __PROGRAM__ [options] __SUBCOMMANDS__
-          __PROGRAM__ --version
-          __PROGRAM__ -h | --help
-
-        Options:
-          -h --help             Show this screen.
-          -V --version          Show version.
-          -q --quiet            Quiet mode.
-      DOCOPT
-
-      def run
-        on_speaker_node do |node|
-          node.stderr = ::StringIO.new if options.fetch('--quiet')
-          if options.fetch('--version')
-            out(::Avm::Tools::VERSION + "\n")
-          else
-            run_with_subcommand
-          end
-        end
+      def application
+        ::Avm::Self.application
       end
     end
   end
