5582447eabbc6d58c1cf1ee798f61005d0c5e1b4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-15 14:49:27 -0300

    Avm::Tools::Runner::EacRedmineBase0::ProjectRename.

---
+++ lib/avm/tools/runner/eac_redmine_base0/project_rename.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'eac_cli/default_runner'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class EacRedmineBase0 < ::EacRubyUtils::Console::DocoptRunner
+        class ProjectRename < ::Avm::Rails::Runners::Runner
+          include ::EacCli::DefaultRunner
+
+          runner_definition do
+            pos_arg :from
+            pos_arg :to
+          end
+
+          def run
+            start_banner
+            command.system!
+          end
+
+          def start_banner
+            infov 'From', from
+            infov 'To', to
+          end
+
+          def from
+            options.fetch('<from>')
+          end
+
+          def to
+            options.fetch('<to>')
+          end
+
+          def command
+            context(:instance).bundle('exec', 'rails', 'runner', code)
+          end
+
+          def code
+            <<~CODE
+              from_arg = '#{from}'
+              to_arg = '#{to}'
+              project = ::Project.where(identifier: from_arg).first
+              if project.present?
+                puts "Project found: \#{project}"
+                puts "Renaming..."
+                project.update_column(:identifier, to_arg)
+                puts "Renamed. Testing..."
+                project = ::Project.where(identifier: to_arg).first
+                if project
+                  puts "Project found: \#{project}"
+                else
+                  fail "After rename: project not found with identifier \\"\#{to_arg}\\""
+                end
+              else
+                fail "Before rename: project not found with identifier \\"\#{from_arg}\\""
+              end
+            CODE
+          end
+        end
+      end
+    end
+  end
+end
