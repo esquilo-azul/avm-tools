2fea508e7674570372b58c0f40f51aed3d9815e1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-24 22:08:42 -0300

    Avm::Tools::Runner::LocalProject::Info.

---
+++ lib/avm/tools/runner/local_project/info.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class LocalProject < ::EacRubyUtils::Console::DocoptRunner
+        class Info < ::EacRubyUtils::Console::DocoptRunner
+          include ::EacCli::DefaultRunner
+
+          runner_definition do
+            desc 'Show information about local project instance.'
+          end
+
+          def run
+            infov 'Path', context(:instance).path
+            infov 'Stereotypes', context(:instance).stereotypes.map(&:label).join(', ')
+          end
+        end
+      end
+    end
+  end
+end
