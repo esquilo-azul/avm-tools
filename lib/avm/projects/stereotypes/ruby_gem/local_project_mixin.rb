a21c98d2addf101651770073f47626e9a5da1c6a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-25 21:06:50 -0300

    Avm::Projects::Stereotypes::RubyGem::LocalProjectMixin.

---
+++ lib/avm/projects/stereotypes/ruby_gem/local_project_mixin.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_gems_utils/gem'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Projects
+    module Stereotypes
+      class RubyGem
+        module LocalProjectMixin
+          # @return [EacRubyGemsUtils::Gem]
+          def ruby_gem
+            @ruby_gem ||= ::EacRubyGemsUtils::Gem.new(path)
+          end
+        end
+      end
+    end
+  end
+end
