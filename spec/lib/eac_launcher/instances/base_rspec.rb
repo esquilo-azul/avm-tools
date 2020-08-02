292f6920dc2340e62cb1c3a361c0101e32abdbfc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-02 11:28:28 -0300

    RSpec.describe(EacLauncher::Instances::Base): refact.

--- spec/lib/eac_launcher/instances/base_rspec.rb
+++ spec/lib/eac_launcher/instances/base_rspec.rb
@@ -4,19 +4,21 @@ require 'eac_launcher/instances/base'
 
 RSpec.describe ::EacLauncher::Instances::Base do
   describe '#options' do
-    it 'default options' do
-      i = ::EacLauncher::Context.current.instance('/eac_launcher_stub')
-      expect(i).to be_instance_of(described_class)
-      expect(i.options.git_current_revision).to eq('origin/master')
-      expect(i.options.git_publish_remote).to eq('publish')
-      expect(i.options.publishable?).to eq(true)
+    context 'when instance is "eac_launcher_stub"' do
+      let(:instance) { ::EacLauncher::Context.current.instance('/eac_launcher_stub') }
+
+      it { expect(instance).to be_a(described_class) }
+      it { expect(instance.options.git_current_revision).to eq('origin/master') }
+      it { expect(instance.options.git_publish_remote).to eq('publish') }
+      it { expect(instance.options.publishable?).to eq(true) }
     end
 
-    it 'options setted' do
-      i = ::EacLauncher::Context.current.instance('/ruby_gem_stub')
-      expect(i.options.git_current_revision).to eq('git_current_revision_setted')
-      expect(i.options.git_publish_remote).to eq('git_publish_remote_setted')
-      expect(i.options.publishable?).to eq(false)
+    context 'when instance is "ruby_gem_stub"' do
+      let(:instance) { ::EacLauncher::Context.current.instance('/ruby_gem_stub') }
+
+      it { expect(instance.options.git_current_revision).to eq('git_current_revision_setted') }
+      it { expect(instance.options.git_publish_remote).to eq('git_publish_remote_setted') }
+      it { expect(instance.options.publishable?).to eq(false) }
     end
   end
 end
