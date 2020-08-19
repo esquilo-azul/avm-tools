d1d1e63f19cd398182281875894034e90054c715
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-19 10:00:14 -0300

    Avm::EacRedmineBase0::DataUnit.do_load.

--- lib/avm/eac_redmine_base0/data_unit.rb
+++ lib/avm/eac_redmine_base0/data_unit.rb
@@ -2,6 +2,7 @@
 
 require 'avm/data/instance/unit'
 require 'eac_ruby_utils/core_ext'
+require 'curb'
 require 'open-uri'
 
 module Avm
@@ -15,6 +16,25 @@ module Avm
         ::File.open(data_path, 'wb') do |file|
           file << URI.parse(export_url).read
         end
+      end
+
+      def do_load(data_path)
+        do_load_by_web(data_path) || do_load_by_rake(data_path) || raise('Failed to load')
+      end
+
+      def do_load_by_web(data_path)
+        c = Curl::Easy.new(import_url)
+        c.multipart_form_post = true
+        c.http_post(Curl::PostField.file('redmine_with_git_tableless_load[path]', data_path))
+        c.perform
+        true
+      rescue Curl::Err::ConnectionFailedError
+        false
+      end
+
+      def do_load_by_rake(data_path)
+        instance.bundle('exec', 'rake', "redmine_with_git:load:all[#{data_path}]").system
+        true
       end
 
       def export_url
