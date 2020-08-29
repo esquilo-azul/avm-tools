b146346ecdfffdd6ac0d9b8a7cfbaa4b94314ae9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-29 11:59:39 -0300

    Avm::Tools::Runner::LocalProject::Ruby.

---
+++ lib/avm/tools/runner/local_project/ruby.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'avm/patches/eac_ruby_gems_utils/gem'
+require 'eac_cli/default_runner'
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class LocalProject < ::EacRubyUtils::Console::DocoptRunner
+        class Ruby < ::EacRubyUtils::Console::DocoptRunner
+          require_sub __FILE__
+          include ::EacCli::DefaultRunner
+
+          runner_definition do
+            desc 'Ruby utitilies for local projects.'
+            subcommands
+          end
+        end
+      end
+    end
+  end
+end
