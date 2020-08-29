69aa4b0c9db392b047ac98dd49586ee95d0bf324
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-29 16:30:23 -0300

    Avm::LocalProjects::Instance.run_job: convert from Avm::LocalProjects::Jobs::Update.

--- lib/avm/local_projects/instance.rb
+++ lib/avm/local_projects/instance.rb
@@ -22,11 +22,25 @@ module Avm
         ::EacLauncher::Paths::Real.new(path.to_path)
       end
 
+      def run_job(job, job_args = [])
+        stereotypes.each { |stereotype| run_stereotype_job(stereotype, job, job_args) }
+      end
+
       private
 
       # @return [Avm::Instances::Configuration]
       def configuration_uncached
         ::Avm::Instances::Configuration.find_in_path(path)
+      end
+
+      def run_stereotype_job(stereotype, job, job_args)
+        job_class_method = "#{job}_class"
+        if stereotype.send(job_class_method).present?
+          puts stereotype.label + ": #{job} class found. Running..."
+          stereotype.send(job_class_method).new(self, *job_args).run
+        else
+          puts stereotype.label + ": #{job} class not found"
+        end
       end
 
       def stereotypes_uncached
--- lib/avm/tools/runner/local_project/update.rb
+++ lib/avm/tools/runner/local_project/update.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'avm/local_projects/jobs/update'
 require 'eac_ruby_utils/console/docopt_runner'
 require 'eac_ruby_utils/core_ext'
 
@@ -17,7 +16,7 @@ module Avm
 
           def run
             infov 'Path', context(:instance).path
-            ::Avm::LocalProjects::Jobs::Update.new(context(:instance)).run
+            context(:instance).run_job(:update)
           end
         end
       end
