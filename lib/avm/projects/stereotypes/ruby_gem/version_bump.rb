ad57006f59fca372a2876ffacab756f3f140ae65
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-29 17:37:43 -0300

    Avm::Projects::Stereotypes::RubyGem::VersionBump.

---
+++ lib/avm/projects/stereotypes/ruby_gem/version_bump.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'avm/patches/object/i18n'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Projects
+    module Stereotypes
+      class RubyGem
+        class VersionBump
+          enable_console_speaker
+          common_constructor :instance, :target_version
+
+          def run
+            git_checkout
+            change_version
+            bundle_run
+            git_commit
+          end
+
+          private
+
+          def bundle_run
+            infom 'Running "bundle install"...'
+            instance.ruby_gem.bundle('install').execute!
+          end
+
+          def change_version
+            infom 'Setting project version...'
+            instance.version = target_version
+          end
+
+          def git_checkout
+            return unless instance.respond_to?(:git_repo)
+
+            infom "Checkouting #{changing_files.map(&:to_path).join(', ')}..."
+            instance.git_repo.command('checkout', '--',
+                                      *changing_files.map(&:to_path)).execute!
+          end
+
+          def git_commit
+            return unless instance.respond_to?(:git_repo)
+
+            infom "Commiting #{changing_files.map(&:to_path).join(', ')}..."
+            instance.git_repo.command('commit', '-m', git_commit_message, '--',
+                                      *changing_files.map(&:to_path)).execute!
+          end
+
+          def git_commit_message
+            ::I18n.default_locale = instance.locale
+            translate(__method__, version: target_version)
+          end
+
+          def changing_files
+            [instance.ruby_gem.gemfile_lock_path, instance.ruby_gem.version_file_path]
+          end
+        end
+      end
+    end
+  end
+end
