1906556ede7831ed5e08b2f8e680253990782328
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-30 12:36:15 -0300

    Avm::Patches::Object::Template: use Avm::Self.root.

--- lib/avm/patches/object/template.rb
+++ lib/avm/patches/object/template.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
+require 'avm/self'
 require 'eac_ruby_utils/patches/object/template'
 
-::EacRubyUtils::Templates::Searcher.default.included_paths << ::File.expand_path(
-  ('../' * 4) + 'template', __dir__
-)
+::EacRubyUtils::Templates::Searcher.default.included_paths <<
+  ::Avm::Self.root.join('template').to_path
