7a5204f7ab7f879b527d5bda5f7ea77ee092bdb8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-30 16:16:36 -0300

    Avm::Tools::Runner::LocalProject::Test.

---
+++ lib/avm/tools/runner/local_project/test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class LocalProject < ::EacRubyUtils::Console::DocoptRunner
+        class Test < ::EacRubyUtils::Console::DocoptRunner
+          include ::EacCli::DefaultRunner
+
+          runner_definition do
+            desc 'Test local project.'
+          end
+
+          def run
+            context(:instance_banner)
+            infov 'Test command', test_command
+            if test_command.present?
+              test_command.system!
+            else
+              fatal_error 'No test command found'
+            end
+          end
+
+          def test_command
+            context(:instance).configuration.if_present(&:any_test_command)
+          end
+        end
+      end
+    end
+  end
+end
