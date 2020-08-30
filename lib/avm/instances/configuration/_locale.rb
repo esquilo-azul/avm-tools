3483b2372c3d973c042f84688356baf2dcf50adb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-30 12:52:12 -0300

    Avm::Instances::Configuration.locale.

---
+++ lib/avm/instances/configuration/_locale.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'avm/patches/eac_ruby_gems_utils/gem'
+require 'i18n'
+
+module Avm
+  module Instances
+    class Configuration < ::EacRubyUtils::Configs
+      LOCALE_KEY = :locale
+
+      def locale
+        read_entry(LOCALE_KEY) || ::I18n.default_locale
+      end
+    end
+  end
+end
