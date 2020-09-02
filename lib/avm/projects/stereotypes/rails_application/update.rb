ecf3e9b8871d16a5b32dbb9bf91b027a95b55fcd
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-09-02 15:43:01 -0300

    Avm::Projects::Stereotypes::RailsApplication::Update.

---
+++ lib/avm/projects/stereotypes/rails_application/update.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'avm/projects/stereotypes/ruby_gem/update'
+
+module Avm
+  module Projects
+    module Stereotypes
+      class RailsApplication
+        class Update < ::Avm::Projects::Stereotypes::RubyGem::Update
+        end
+      end
+    end
+  end
+end
