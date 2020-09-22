80ee94a04d60b68fde5b3b8ce3b4a813cc65d99c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-09-22 15:12:31 -0300

    Avm::Tools::Runner::EacWordpressBase0::ApacheHost: refact with EacCli.

--- lib/avm/tools/runner/eac_wordpress_base0/apache_host.rb
+++ lib/avm/tools/runner/eac_wordpress_base0/apache_host.rb
@@ -1,26 +1,19 @@
 # frozen_string_literal: true
 
+require 'eac_cli/default_runner'
 require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/console/speaker'
 require 'avm/stereotypes/eac_wordpress_base0/apache_host'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class EacWordpressBase0 < ::EacRubyUtils::Console::DocoptRunner
-        class ApacheHost < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacRubyUtils::Console::Speaker
+        class ApacheHost
+          include ::EacCli::DefaultRunner
 
-          DOC = <<~DOCOPT
-            Configure Apache virtual host for EacWordpressBase0 instance.
-
-            Usage:
-              __PROGRAM__ [options]
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                       Show this screen.
-          DOCOPT
+          runner_definition do
+            desc 'Configure Apache virtual host for EacWordpressBase0 instance.'
+          end
 
           def run
             result = ::Avm::Stereotypes::EacWordpressBase0::ApacheHost.new(
