869652bf1857e9f08d37126a37168c648cf31b55
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-09-22 15:43:05 -0300

    Avm::Tools::Runner::EacRailsBase0: use Avm::EacWebappBase0::Runner::ApacheHost.

--- lib/avm/tools/runner/eac_rails_base0/apache_host.rb
+++ lib/avm/tools/runner/eac_rails_base0/apache_host.rb
@@ -1,37 +1,14 @@
 # frozen_string_literal: true
 
+require 'avm/eac_webapp_base0/runner/apache_host'
 require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/console/speaker'
 require 'avm/eac_rails_base0/apache_host'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       class EacRailsBase0 < ::EacRubyUtils::Console::DocoptRunner
-        class ApacheHost < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacRubyUtils::Console::Speaker
-
-          DOC = <<~DOCOPT
-            Configure Apache virtual host for EacRailsBase0 instance.
-
-            Usage:
-              __PROGRAM__ [options]
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                       Show this screen.
-          DOCOPT
-
-          def run
-            result = ::Avm::EacRailsBase0::ApacheHost.new(
-              context(:instance)
-            ).run
-            if result.error?
-              fatal_error result.to_s
-            else
-              infov 'Result', result.label
-            end
-          end
+        class ApacheHost < ::Avm::EacWebappBase0::Runner::ApacheHost
         end
       end
     end
