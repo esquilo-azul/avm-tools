db25628bab810d829be9b738ece4096c55fefdc1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-14 16:33:01 -0300

    Avm::Rails::Instance.the_gem: refact .bundle.

--- lib/avm/rails/instance.rb
+++ lib/avm/rails/instance.rb
@@ -4,14 +4,15 @@ module Avm
   module Rails
     module Instance
       def bundle(*args)
-        host_env.command('bundle', *args)
-                .envvar('BUNDLE_GEMFILE', ::File.join(read_entry('fs_path'), 'Gemfile'))
-                .envvar('RAILS_ENV', 'production')
-                .chdir(read_entry('fs_path'))
+        the_gem.bundle(*args).chdir_root.envvar('RAILS_ENV', 'production')
       end
 
       def rake(*args)
         bundle('exec', 'rake', *args)
+      end
+
+      def the_gem
+        @the_gem ||= ::EacRubyGemsUtils::Gem.new(::File.join(read_entry('fs_path')), host_env)
       end
     end
   end
