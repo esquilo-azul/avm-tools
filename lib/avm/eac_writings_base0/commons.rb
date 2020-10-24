40ac5c8c0628be642020e52f8f1aa3bfecf574ba
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-24 15:39:36 -0300

    Avm::EacWritingsBase0::Commons.

---
+++ lib/avm/eac_writings_base0/commons.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'avm/patches/object/template'
+require 'singleton'
+
+module Avm
+  module EacWritingsBase0
+    class Commons
+      include ::Singleton
+    end
+  end
+end
