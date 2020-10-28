5d90928b7fbba28a2e012e0ca3b20b04fd3d5c81
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-28 16:43:57 -0300

    Avm::Tools::Runner: refact.

--- lib/avm/tools/runner.rb
+++ lib/avm/tools/runner.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/require_sub'
-::EacRubyUtils.require_sub(__FILE__)
 require 'avm/self'
 require 'avm/tools/version'
 require 'eac_ruby_base0/runner'
+require 'eac_ruby_utils/console/docopt_runner'
+require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
       include ::EacRubyBase0::Runner
+      require_sub __FILE__
 
       runner_definition do
         desc 'Tools for AVM.'
