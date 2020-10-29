222679b8d09e6817ffd8ba30ec2ee15db3a9bfd6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-29 15:53:37 -0300

    Avm::EacRailsBase1::Runner: convert from Avm::Rails::Runners.

--- lib/avm/eac_rails_base1/runner.rb
+++ lib/avm/eac_rails_base1/runner.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+require 'avm/eac_webapp_base0/runner'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
-  module Rails
-    module Runners
+  module EacRailsBase1
+    class Runner < ::Avm::EacWebappBase0::Runner
       require_sub __FILE__
     end
   end
