b959c0860697fedb3b5d6653f37ebe8a81386b36
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-29 20:34:52 -0300

    Avm::EacRailsBase1::Runner::CodeRunner: refact with "eac_cli".

--- lib/avm/eac_rails_base1/runner/code_runner.rb
+++ lib/avm/eac_rails_base1/runner/code_runner.rb
@@ -1,27 +1,19 @@
 # frozen_string_literal: true
 
+require 'eac_cli/core_ext'
 require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/core_ext'
 
 module Avm
   module EacRailsBase1
     class Runner < ::Avm::EacWebappBase0::Runner
       class CodeRunner < ::EacRubyUtils::Console::DocoptRunner
-        enable_console_speaker
-        enable_simple_cache
-
-        DOC = <<~DOC
-          Runs a Ruby code with "rails runner".
-
-          Usage:
-            __PROGRAM__ [options] <code>
-            __PROGRAM__ -h | --help
-
-          Options:
-            -h --help         Show this screen.
-            -e --environment  Specifies the environment for the runner to
-                              operate (test/development/production). Default: "development".
-        DOC
+        runner_with
+        runner_definition do
+          desc 'Runs a Ruby code with "rails runner".'
+          arg_opt '-e', '--environment', 'Specifies the environment for the runner to operate' \
+            ' (test/development/production). Default: "development".'
+          pos_arg :code
+        end
 
         def run
           infov 'Runner arguments', runner_args
