168a7e92e444e3c303f88924f134ce147526f1b9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-29 15:48:56 -0300

    Avm::Tools::Runner::EacWordpressBase0: refact with Avm::EacWebappBase0::Runner.

--- lib/avm/tools/runner/eac_wordpress_base0.rb
+++ lib/avm/tools/runner/eac_wordpress_base0.rb
@@ -1,34 +1,15 @@
 # frozen_string_literal: true
 
+require 'avm/eac_webapp_base0/runner'
+require 'avm/eac_wordpress_base0'
 require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/simple_cache'
-require 'avm/eac_wordpress_base0/instance'
-require 'avm/tools/runner/eac_wordpress_base0/apache_host'
-require 'avm/tools/runner/eac_wordpress_base0/data'
-require 'avm/tools/runner/eac_wordpress_base0/deploy'
+require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Tools
     class Runner < ::EacRubyUtils::Console::DocoptRunner
-      class EacWordpressBase0 < ::EacRubyUtils::Console::DocoptRunner
-        include ::EacRubyUtils::SimpleCache
-
-        DOC = <<~DOCOPT
-          Utilities for EacWordpressBase0 instances.
-
-          Usage:
-            __PROGRAM__ [options] <instance_id> __SUBCOMMANDS__
-            __PROGRAM__ -h | --help
-
-          Options:
-            -h --help             Show this screen.
-        DOCOPT
-
-        private
-
-        def instance_uncached
-          ::Avm::EacWordpressBase0::Instance.by_id(options['<instance_id>'])
-        end
+      class EacWordpressBase0 < ::Avm::EacWebappBase0::Runner
+        require_sub __FILE__
       end
     end
   end
