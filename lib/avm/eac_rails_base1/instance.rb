7247c9ef9328f9605a0fadc0543b1e44cacc04b9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-29 21:40:21 -0300

    Avm::EacRailsBase1::Instance::DEFAULT_RAILS_ENVIRONMENT.

--- lib/avm/eac_rails_base1/instance.rb
+++ lib/avm/eac_rails_base1/instance.rb
@@ -6,8 +6,10 @@ require 'eac_ruby_gems_utils/gem'
 module Avm
   module EacRailsBase1
     class Instance < ::Avm::EacWebappBase0::Instance
+      DEFAULT_RAILS_ENVIRONMENT = 'production'
+
       def bundle(*args)
-        the_gem.bundle(*args).chdir_root.envvar('RAILS_ENV', 'production')
+        the_gem.bundle(*args).chdir_root.envvar('RAILS_ENV', DEFAULT_RAILS_ENVIRONMENT)
       end
 
       def rake(*args)
