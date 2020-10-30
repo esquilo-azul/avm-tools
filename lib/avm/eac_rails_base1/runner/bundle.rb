85c8fa2da84fc40fa1f58efbf648e89ccf5bf156
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-29 21:23:43 -0300

    Avm::EacRailsBase1::Runner::Bundle: runner with Avm::EacRailsBase1::RunnerWith::Bundle.

--- lib/avm/eac_rails_base1/runner/bundle.rb
+++ lib/avm/eac_rails_base1/runner/bundle.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'avm/eac_rails_base1/runner_with/bundle'
 require 'eac_cli/core_ext'
 require 'eac_ruby_utils/console/docopt_runner'
 require 'shellwords'
@@ -8,15 +9,14 @@ module Avm
   module EacRailsBase1
     class Runner < ::Avm::EacWebappBase0::Runner
       class Bundle < ::EacRubyUtils::Console::DocoptRunner
-        runner_with
+        runner_with ::Avm::EacRailsBase1::RunnerWith::Bundle
         runner_definition do
           desc 'Runs "bundle ...".'
           pos_arg :'bundle-args', repeat: true, optional: true
         end
 
         def run
-          infov 'Bundle arguments', ::Shellwords.join(bundle_args)
-          context(:instance).bundle(*bundle_args).system!
+          bundle_run
         end
 
         def bundle_args
