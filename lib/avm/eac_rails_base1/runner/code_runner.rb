ca03a60d61707b435acb79fb331dcb512391aba0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-29 21:20:39 -0300

    Avm::EacRailsBase1::Runner::CodeRunner: runner with Avm::EacRailsBase1::RunnerWith::Bundle.

--- lib/avm/eac_rails_base1/runner/code_runner.rb
+++ lib/avm/eac_rails_base1/runner/code_runner.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'avm/eac_rails_base1/runner_with/bundle'
 require 'eac_cli/core_ext'
 require 'eac_ruby_utils/console/docopt_runner'
 
@@ -7,31 +8,19 @@ module Avm
   module EacRailsBase1
     class Runner < ::Avm::EacWebappBase0::Runner
       class CodeRunner < ::EacRubyUtils::Console::DocoptRunner
-        runner_with
+        runner_with ::Avm::EacRailsBase1::RunnerWith::Bundle
         runner_definition do
           desc 'Runs a Ruby code with "rails runner".'
-          arg_opt '-e', '--environment', 'Specifies the environment for the runner to operate' \
-            ' (test/development/production). Default: "development".'
           pos_arg :code
         end
 
         def run
-          infov 'Runner arguments', runner_args
           infov 'Environment', context(:instance).host_env
-          runner_command.system!
-        end
-
-        def runner_command_uncached
-          context(:instance).bundle(*bundle_args)
+          bundle_run
         end
 
         def bundle_args
-          %w[exec rails runner] + runner_args
-        end
-
-        def runner_args
-          options.fetch('--environment').if_present([]) { |v| ['--environment', v] } +
-            [options.fetch('<code>')]
+          %w[exec rails runner] + [options.fetch('<code>')]
         end
       end
     end
