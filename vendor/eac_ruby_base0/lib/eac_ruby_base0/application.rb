f6e4645272f42a377b83195ace38abd8ccfc142a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-02 00:30:38 -0300

    Gem "eac_ruby_base0" (Local): update to '0.3.0'.

--- vendor/eac_ruby_base0/lib/eac_ruby_base0/application.rb
+++ vendor/eac_ruby_base0/lib/eac_ruby_base0/application.rb
@@ -1,18 +1,49 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/filesystem_cache'
 
 module EacRubyBase0
   class Application
     enable_simple_cache
-    common_constructor :gemspec_dir do
+    enable_listable
+    lists.add_symbol :option, :name, :home_dir
+
+    common_constructor :gemspec_dir, :options, default: [{}] do
       self.gemspec_dir = gemspec_dir.to_pathname
+      self.options = options.symbolize_keys.assert_valid_keys(self.class.lists.option.values).freeze
     end
 
     delegate :version, to: :self_gem
 
     def all_gems
       vendor_gems + [self_gem]
+    end
+
+    { cache: '.cache', config: '.config', data: '.local/share' }.each do |item, subpath|
+      xdg_env_method_name = "#{item}_xdg_env"
+
+      define_method xdg_env_method_name do
+        ENV["XDG_#{item.upcase}_HOME"].if_present(&:to_pathname)
+      end
+
+      define_method "#{item}_dir" do
+        (send(xdg_env_method_name) || home_dir.join(subpath)).join(name)
+      end
+    end
+
+    def fs_cache
+      @fs_cache ||= ::EacRubyUtils::FilesystemCache.new(
+        cache_dir.join(::EacRubyUtils::FilesystemCache.name.parameterize)
+      )
+    end
+
+    def home_dir
+      @home_dir ||= (options[OPTION_HOME_DIR] || ENV.fetch('HOME')).to_pathname
+    end
+
+    def name
+      options[OPTION_NAME] || self_gem.name
     end
 
     def vendor_dir
