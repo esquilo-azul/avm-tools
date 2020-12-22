5f7d286128869a652ade2b9088dfd7b89ac27ff9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-22 16:06:54 -0300

    Avm::EacWritingsBase1::Build::File.

---
+++ lib/avm/eac_writings_base1/build/file.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'avm/executables'
+
+module Avm
+  module EacWritingsBase1
+    class Build
+      class File
+        enable_console_speaker
+        common_constructor :build, :subpath
+
+        def run
+          infov 'Building', subpath
+          target_path.parent.mkpath
+          ::Avm::Executables.asciidoc.command('--out-file', target_path, source_path).system!
+        end
+
+        def source_path
+          build.project.root.join(subpath)
+        end
+
+        def target_path
+          build.target_directory.join(subpath).basename_sub('.*') { |b| "#{b}.html" }
+        end
+      end
+    end
+  end
+end
