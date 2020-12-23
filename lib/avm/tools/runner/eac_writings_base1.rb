ec4de86be97a9e0bd5d63db907dedd96a1d771fa
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-23 13:13:51 -0300

    Avm::Tools::Runner::EacWritingsBase1.

---
+++ lib/avm/tools/runner/eac_writings_base1.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'avm/eac_webapp_base0/runner'
+require 'avm/eac_writings_base1'
+require 'eac_ruby_utils/console/docopt_runner'
+
+module Avm
+  module Tools
+    class Runner < ::EacRubyUtils::Console::DocoptRunner
+      class EacWritingsBase1 < ::Avm::EacWebappBase0::Runner
+        require_sub __FILE__
+      end
+    end
+  end
+end
