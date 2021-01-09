3b2ed97b7ec7c950b6e85797316128dcfc69c4e6
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-01-09 13:24:16 -0300

    Avm::Tools::Runner::Files::Format: refact with EacCli.

--- lib/avm/tools/runner/files/format.rb
+++ lib/avm/tools/runner/files/format.rb
@@ -1,40 +1,31 @@
 # frozen_string_literal: true
 
 require 'avm/files/formatter'
+require 'eac_cli/core_ext'
 require 'eac_launcher/git/base'
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Tools
     class Runner
       class Files
-        class Format < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacRubyUtils::Console::Speaker
-
-          DOC = <<~DOCOPT
-            Format files.
-
-            Usage:
-              __PROGRAM__ [options] [<paths>...]
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                 Show this screen.
-              -a --apply                Confirm changes.
-              -n --no-recursive         No recursive.
-              -v --verbose              Verbose
-              -d --git-dirty            Select Git dirty files to format.
-          DOCOPT
+        class Format
+          runner_with :help do
+            desc 'Format files.'
+            bool_opt '-a', '--apply', 'Confirm changes.'
+            bool_opt '-n', '--no-recursive', 'No recursive.'
+            bool_opt '-v', '--verbose', 'Verbose'
+            bool_opt '-d', '--git-dirty', 'Select Git dirty files to format.'
+            pos_arg :paths, repeat: true, optional: true
+          end
 
           def run
             ::Avm::Files::Formatter.new(source_paths, formatter_options).run
           end
 
           def formatter_options
-            { ::Avm::Files::Formatter::OPTION_APPLY => options.fetch('--apply'),
-              ::Avm::Files::Formatter::OPTION_RECURSIVE => !options.fetch('--no-recursive'),
-              ::Avm::Files::Formatter::OPTION_VERBOSE => options.fetch('--verbose') }
+            { ::Avm::Files::Formatter::OPTION_APPLY => parsed.apply?,
+              ::Avm::Files::Formatter::OPTION_RECURSIVE => !parsed.no_recursive?,
+              ::Avm::Files::Formatter::OPTION_VERBOSE => parsed.verbose? }
           end
 
           def git
@@ -46,10 +37,10 @@ module Avm
           end
 
           def source_paths
-            if options.fetch('--git-dirty')
-              options.fetch('<paths>') + git_dirty_files
+            if parsed.git_dirty?
+              parsed.paths + git_dirty_files
             else
-              options.fetch('<paths>').if_present(%w[.])
+              parsed.paths.if_present(%w[.])
             end
           end
         end
