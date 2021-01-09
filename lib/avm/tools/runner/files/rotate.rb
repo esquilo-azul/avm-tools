65b9c1c5422547e805285e32a65ebfd42bcb9dbe
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-01-09 14:16:12 -0300

    Avm::Tools::Runner::Files::Rotate: refact with EacCli.

--- lib/avm/tools/runner/files/rotate.rb
+++ lib/avm/tools/runner/files/rotate.rb
@@ -1,28 +1,18 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/console/speaker'
 require 'avm/files/rotate'
+require 'eac_cli/core_ext'
 
 module Avm
   module Tools
     class Runner
       class Files
-        class Rotate < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacRubyUtils::Console::Speaker
-
-          DOC = <<~DOCOPT
-            Rotates a file (Like a backup).
-
-            Usage:
-              __PROGRAM__ [options] <path>
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                 Show this screen.
-              -L --space-limit=<space>  Limit by <space> the space used by rotated files.
-          DOCOPT
-
+        class Rotate
+          runner_with :help do
+            desc 'Rotates a file (Like a backup).'
+            arg_opt '-L', '--space-limit', 'Limit by <space> the space used by rotated files.'
+            pos_arg :path
+          end
           def run
             error_message = rotate.run
             fatal_error(error_message) if error_message
@@ -30,8 +20,8 @@ module Avm
 
           def rotate
             @rotate ||= ::Avm::Files::Rotate.new(
-              options.fetch('<path>'),
-              space_limit: options.fetch('--space-limit')
+              parsed.path,
+              space_limit: parsed.space_limit
             )
           end
         end
