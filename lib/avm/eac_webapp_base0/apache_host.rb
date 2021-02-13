4897f57c8e6717023c620df9d9e4f372882464d9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-13 18:22:54 -0300

    Avm::EacWebappBase0::ApacheHost: refact with Avm::Apps::Jobs::Base.

--- lib/avm/eac_webapp_base0/apache_host.rb
+++ lib/avm/eac_webapp_base0/apache_host.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/core_ext'
+require 'avm/apps/jobs/base'
 require 'avm/patches/object/template'
 require 'avm/eac_ubuntu_base0/apache'
 require 'avm/patches/object/template'
@@ -8,24 +9,14 @@ require 'avm/patches/object/template'
 module Avm
   module EacWebappBase0
     class ApacheHost
-      enable_console_speaker
-      enable_simple_cache
-      common_constructor :instance, :options, default: [{}]
-
-      def run
-        write_available_no_ssl_site
-        enable_no_ssl_site
-        remove_ssl_site
-        reload_apache
-        run_certbot
-        enable_ssl_site
-        reload_apache
-        ::Avm::Result.success('Done')
-      end
+      JOBS = %w[write_available_no_ssl_site enable_no_ssl_site remove_ssl_site reload_apache
+                run_certbot enable_ssl_site reload_apache].freeze
+      include ::Avm::Apps::Jobs::Base
 
       def no_ssl_site_content
-        ::Avm::EacWebappBase0::ApacheHost.template.child('no_ssl.conf')
-                                         .apply(EntriesReader.new(self, instance))
+        ::Avm::EacWebappBase0::ApacheHost
+          .template.child('no_ssl.conf')
+          .apply(variables_source)
       end
 
       def ssl?
@@ -82,21 +73,6 @@ module Avm
       def write_available_no_ssl_site
         infom 'Writing no SSL site conf...'
         no_ssl_site.write(no_ssl_site_content)
-      end
-
-      class EntriesReader
-        common_constructor :job, :instance
-
-        def read_entry(path, options = {})
-          entry_from_job(path) || instance.read_entry(path, options)
-        end
-
-        private
-
-        def entry_from_job(path)
-          method = path.gsub('.', '_').underscore
-          return job.send(method) if job.respond_to?(method, true)
-        end
       end
     end
   end
