b8f0081bec00847708f83b34f18487b30fb2fefb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-13 16:57:13 -0300

    Avm::EacWebappBase0::Runner::Data::Dump: refact with EacCli.

--- lib/avm/eac_webapp_base0/runner/data/dump.rb
+++ lib/avm/eac_webapp_base0/runner/data/dump.rb
@@ -1,37 +1,26 @@
 # frozen_string_literal: true
 
-require 'active_support/core_ext/numeric/time'
-require 'eac_ruby_utils/console/speaker'
-require 'eac_ruby_utils/simple_cache'
 require 'avm/data/package/dump'
+require 'eac_cli/core_ext'
 
 module Avm
   module EacWebappBase0
     class Runner < ::Avm::Instances::Runner
       class Data
-        class Dump < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacRubyUtils::SimpleCache
-          include ::EacRubyUtils::Console::Speaker
-
+        class Dump
           DUMP_EXPIRE_TIME = 1.day
           DEFAULT_DUMP_PATH_ENTRY_SUFFIX = 'data.default_dump_path'
           NO_DUMP_MESSAGE = 'Dump "%s" already exist and rewrite options was no setted nor ' \
             'dump was expired.'
 
-          DOC = <<~DOCUMENT
-            Dump utility for EacRailsBase instance.
-
-                Usage:
-                __PROGRAM__ [options]
-
-              Options:
-                -h --help               Show this screen.
-                --rewrite               Forces dump overwrite.
-                --dump-path=<dump_path> Set DUMP_PATH variable.
-          DOCUMENT
+          runner_with :help do
+            desc 'Dump utility for EacRailsBase instance.'
+            bool_opt '-w', 'rewrite', 'Forces dump overwrite.'
+            arg_opt '-p', '--dump-path', 'Set DUMP_PATH variable.'
+          end
 
           def run
-            infov 'Instance to dump', "#{context(:instance)} (#{context(:instance).class})"
+            infov 'Instance to dump', "#{instance} (#{instance.class})"
             if package_dump.runnable?
               package_dump.run
             else
@@ -43,20 +32,25 @@ module Avm
 
           private
 
+          def instance
+            runner_context.call(:instance)
+          end
+
           def package_dump_uncached
-            context(:instance).data_package.dump(dump_path, existing: package_dump_existing)
+            instance
+              .data_package.dump(dump_path, existing: package_dump_existing)
           end
 
           def dump_path
-            options.fetch('--dump-path') || default_dump_path
+            parsed.dump_path || default_dump_path
           end
 
           def default_dump_path
-            context(:instance).read_entry(DEFAULT_DUMP_PATH_ENTRY_SUFFIX)
+            instance.read_entry(DEFAULT_DUMP_PATH_ENTRY_SUFFIX)
           end
 
           def package_dump_existing
-            if options.fetch('--rewrite')
+            if parsed.rewrite?
               ::Avm::Data::Package::Dump::EXISTING_ROTATE
             else
               ::Avm::Data::Package::Dump::EXISTING_ROTATE_EXPIRED
