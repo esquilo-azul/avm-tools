e50112fd0cf5426462dfaa849e55fd35b2491374
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-11 13:10:15 -0300

    Avm::Docker::Runner: dismiss EacRubyUtils::Console::DocoptRunner.

--- lib/avm/docker/runner.rb
+++ lib/avm/docker/runner.rb
@@ -1,33 +1,25 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/core_ext'
+require 'avm/core_ext'
 require 'avm/docker/registry'
 
 module Avm
   module Docker
-    class Runner < ::EacRubyUtils::Console::DocoptRunner
+    class Runner
       enable_console_speaker
       enable_simple_cache
 
-      DOC = <<~DOCOPT
-        Manipulate Docker images.
-
-        Usage:
-          __PROGRAM__ [options] [-B <build-arg>...] [-E <run-arg>...]
-          __PROGRAM__ -h | --help
-
-        Options:
-          -h --help     Show this help.
-          -n --registry-name=<registry-name>    Docker registry's name.
-          -p --push     Push the image to Docker registry.
-          -r --run      Run or start a container with builded image.
-          -B --build-arg=<build-arg>  Argument for "docker build".
-          -E --entrypoint-arg=<run-arg>    Argument for entrypoint on "docker run"
-          -c --clear  Remove container if exist before run.
-          -S --no-snapshot    Does not add "-snapshot" to image tag.
-          -V --no-version   Does not add version to image tag.
-      DOCOPT
+      runner_with :help do
+        desc 'Manipulate Docker images.'
+        arg_opt '-n', '--registry-name', 'Docker registry\'s name.'
+        bool_opt '-p', '--push', 'Push the image to Docker registry.'
+        bool_opt '-r', '--run', 'Run or start a container with builded image.'
+        arg_opt '-B', '--build-arg', 'Argument for "docker build".', repeat: true
+        arg_opt '-E', '--entrypoint-arg', 'Argument for entrypoint on "docker run"', repeat: true
+        bool_opt '-c', '--clear', 'Remove container if exist before run.'
+        bool_opt '-S', '--no-snapshot', 'Does not add "-snapshot" to image tag.'
+        bool_opt '-V', '--no-version', 'Does not add version to image tag.'
+      end
 
       def run
         setup
@@ -61,42 +53,42 @@ module Avm
       end
 
       def build_args
-        options.fetch('--build-arg')
+        parsed.build_arg
       end
 
       def entrypoint_args
-        options.fetch('--entrypoint-arg')
+        parsed.entrypoint_arg
       end
 
       def push
-        instance.docker_image.push if options.fetch('--push')
+        instance.docker_image.push if parsed.push?
       end
 
       def container_run
-        return unless options.fetch('--run')
+        return unless parsed.run?
 
         instance.docker_container.run(
           entrypoint_args: entrypoint_args,
-          clear: options.fetch('--clear')
+          clear: parsed.clear?
         )
       end
 
       def registry_uncached
-        options.fetch('--registry-name').if_present(::Avm::Docker::Registry.default) do |v|
+        parsed.registry_name.if_present(::Avm::Docker::Registry.default) do |v|
           ::Avm::Docker::Registry.new(v)
         end
       end
 
       def snapshot?
-        !options.fetch('--no-snapshot')
+        !parsed.no_snapshot?
       end
 
       def version?
-        !options.fetch('--no-version')
+        !parsed.no_version?
       end
 
       def instance
-        context(:instance)
+        runner_context.call(:instance)
       end
     end
   end
