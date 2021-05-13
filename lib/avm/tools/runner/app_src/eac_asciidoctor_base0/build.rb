26af8f0ac9130be8978934bdb8c5ae1b129aa323
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-13 15:36:00 -0300

    Avm::Runner::AppSrc::EacAsciidoctorBase0::Build: option "--open".

--- lib/avm/tools/runner/app_src/eac_asciidoctor_base0/build.rb
+++ lib/avm/tools/runner/app_src/eac_asciidoctor_base0/build.rb
@@ -2,6 +2,7 @@
 
 require 'avm/core_ext'
 require 'avm/eac_asciidoctor_base0/sources/build'
+require 'os'
 
 module Avm
   module Tools
@@ -12,11 +13,13 @@ module Avm
             runner_with :help do
               desc 'Build the project'
               arg_opt '-d', '--target-dir', 'Directory to build'
+              bool_opt '--open', 'Show the result.'
             end
 
             def run
               start_banner
               build.run
+              open
             end
 
             private
@@ -28,6 +31,17 @@ module Avm
 
             def default_target_directory
               runner_context.call(:project).root.join('build')
+            end
+
+            def open
+              return unless parsed.open?
+
+              infom "Opening \"#{open_path}\"..."
+              ::EacRubyUtils::Envs.local.command(OS.open_file_command, open_path).system!
+            end
+
+            def open_path
+              build.source_files.first.target_path
             end
 
             def start_banner
