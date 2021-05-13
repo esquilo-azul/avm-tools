7afa70d0eabbfca90cfe9c4c9b6c9f9375168caf
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-13 14:44:56 -0300

    Avm::Apps::Sources::Configuration: copy of Avm::Instances::Configuration from "avm-tools".

---
+++ vendor/avm-apps/lib/avm/apps/sources/configuration/_locale.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'avm/patches/eac_ruby_gems_utils/gem'
+require 'i18n'
+
+module Avm
+  module Apps
+    module Sources
+      class Configuration < ::EacRubyUtils::Configs
+        LOCALE_KEY = :locale
+
+        def locale
+          read_entry(LOCALE_KEY) || ::I18n.default_locale
+        end
+      end
+    end
+  end
+end
---
+++ vendor/avm-apps/lib/avm/apps/sources/configuration/_tests.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'avm/patches/eac_ruby_gems_utils/gem'
+
+module Avm
+  module Apps
+    module Sources
+      class Configuration < ::EacRubyUtils::Configs
+        BUNDLE_TEST_COMMAND_KEY = 'test.bundle_command'
+        TEST_COMMAND_KEY = 'test.command'
+
+        def any_test_command
+          bundle_test_command || test_command
+        end
+
+        def test_command
+          read_command(TEST_COMMAND_KEY)
+        end
+
+        def bundle_test_command
+          read_entry(BUNDLE_TEST_COMMAND_KEY).if_present do |v|
+            args = v.is_a?(::Enumerable) ? v.map(&:to_s) : ::Shellwords.split(v)
+            ::EacRubyGemsUtils::Gem.new(::File.dirname(storage_path)).bundle(*args).chdir_root
+          end
+        end
+      end
+    end
+  end
+end
