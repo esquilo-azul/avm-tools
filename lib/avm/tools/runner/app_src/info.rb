edd1b03c9abf8ea54d479f8a443a546158f75462
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-19 14:54:04 -0300

    Avm::Tools::Runner::AppSrc::Info: dismiss EacRubyUtils::Console::DocoptRunner.

--- lib/avm/tools/runner/app_src/info.rb
+++ lib/avm/tools/runner/app_src/info.rb
@@ -1,22 +1,19 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/console/docopt_runner'
-require 'eac_ruby_utils/core_ext'
+require 'avm/core_ext'
 
 module Avm
   module Tools
     class Runner
       class AppSrc
-        class Info < ::EacRubyUtils::Console::DocoptRunner
-          include ::EacCli::DefaultRunner
-
-          runner_definition do
+        class Info
+          runner_with :help do
             desc 'Show information about local project instance.'
           end
 
           def run
-            infov 'Path', context(:instance).path
-            infov 'Stereotypes', context(:instance).stereotypes.map(&:label).join(', ')
+            infov 'Path', runner_context.call(:instance).path
+            infov 'Stereotypes', runner_context.call(:instance).stereotypes.map(&:label).join(', ')
           end
         end
       end
