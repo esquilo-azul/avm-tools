c7647a5a5cc7f295c6f11ab2683ca1b4505e3743
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-05-20 09:54:20 -0300

    Avm::Tools::Runner::Git::DirtyFiles: dismiss EacRubyUtils::Console::DocoptRunner.

--- lib/avm/tools/runner/git/dirty_files.rb
+++ lib/avm/tools/runner/git/dirty_files.rb
@@ -1,46 +1,41 @@
 # frozen_string_literal: true
 
-require 'eac_ruby_utils/core_ext'
-require 'eac_ruby_utils/console/docopt_runner'
+require 'avm/core_ext'
 
 module Avm
   module Tools
     class Runner
       class Git
-        class DirtyFiles < ::EacRubyUtils::Console::DocoptRunner
-          enable_console_speaker
-
+        class DirtyFiles
+          DEFAULT_FORMAT = '%p'
           FIELDS = {
             i: :index, w: :worktree, p: :path, a: :absolute_path
           }.map { |k, v| ["%#{k}", v] }.to_h
 
-          FIELDS_DOC = FIELDS.map { |k, v| "  #{k} => #{v}" }.join("\n")
+          runner_with :help do
+            desc 'Lists dirty files in Git repository.'
+            arg_opt '-f', '--format',
+                    "Format of each line (See \"Format fields\") [default: #{DEFAULT_FORMAT}]."
+          end
 
-          DOC = <<~DOCOPT
-            Lists dirty files in Git repository.
-
-            Usage:
-              __PROGRAM__ [options]
-              __PROGRAM__ -h | --help
-
-            Options:
-              -h --help                     Mostra esta ajuda.
-              -f --format=<format>          Format of each line (See "Format fields") [default: %p].
-
-            Format fields:
-            #{FIELDS_DOC}
-          DOCOPT
+          def help_extra_text
+            "Format fields:\n" + FIELDS.map { |k, v| "  #{k} => #{v}" }.join("\n")
+          end
 
           def run
-            context(:git).dirty_files.each do |file|
+            runner_context.call(:git).dirty_files.each do |file|
               out("#{format_file(file)}\n")
             end
           end
 
           private
 
+          def format
+            parsed.format || DEFAULT_FORMAT
+          end
+
           def format_file(file)
-            FIELDS.inject(options.fetch('--format')) { |a, e| a.gsub(e.first, file.send(e.last)) }
+            FIELDS.inject(format) { |a, e| a.gsub(e.first, file.send(e.last)) }
           end
         end
       end
