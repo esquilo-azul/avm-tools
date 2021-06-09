5499561204e201cfca0929e2f0c8ea85781d2e5a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-06-09 01:36:23 -0300

    Avm::Docker::Registry: extend EacDocker::Registry.

--- lib/avm/docker/registry.rb
+++ lib/avm/docker/registry.rb
@@ -1,27 +1,18 @@
 # frozen_string_literal: true
 
 require 'avm/self/instance'
+require 'eac_docker/registry'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Docker
-    class Registry
+    class Registry < ::EacDocker::Registry
       class << self
         def default
           @default ||= new(::Avm::Self.instance.read_entry(
                              ::Avm::Self::Instance::EntryKeys::DOCKER_REGISTRY_NAME
                            ))
         end
-      end
-
-      common_constructor :name
-
-      def to_s
-        name
-      end
-
-      def sub(suffix)
-        self.class.new("#{name}#{suffix}")
       end
     end
   end
