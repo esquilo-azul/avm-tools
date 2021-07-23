52cb59746654ec23b5d8f3ae6a7fdc18d2278886
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-07-23 11:05:30 -0300

    Avm::Files::*: move from "avm-tools".

---
+++ vendor/avm/lib/avm/files/formatter/formats/php.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'avm/files/formatter/formats/generic_plain'
+
+module Avm
+  module Files
+    class Formatter
+      module Formats
+        class Php < ::Avm::Files::Formatter::Formats::GenericPlain
+          VALID_BASENAMES = %w[*.php].freeze
+          VALID_TYPES = ['x-php'].freeze
+
+          def file_apply(file)
+            ::Avm::Executables.php_cs_fixer.command.append(['fix', file]).system!
+            super(file)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ vendor/avm/lib/avm/files/info.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'avm/executables'
+require 'eac_ruby_utils/core_ext'
+require 'content-type'
+
+module Avm
+  module Files
+    class Info
+      enable_simple_cache
+      common_constructor :path
+
+      private
+
+      def content_type_uncached
+        ::ContentType.parse(
+          ::Avm::Executables.file.command.append(['-ib', path]).execute!.strip
+        )
+      rescue Parslet::ParseFailed
+        ::ContentType.parse('application/unknown')
+      end
+    end
+  end
+end
