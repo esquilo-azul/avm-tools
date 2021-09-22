ff3947d4c96017b24a289975aeda0b8c13284740
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-22 14:05:37 -0300

    Gem "avm" (Local): update to '0.3.2'.

--- vendor/avm/lib/avm/files/formatter/utf8_assert.rb
+++ vendor/avm/lib/avm/files/formatter/utf8_assert.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'avm/files/info'
+
 module Avm
   module Files
     class Formatter
--- vendor/avm/spec/lib/avm/git/auto_commit_path_spec.rb
+++ vendor/avm/spec/lib/avm/git/auto_commit_path_spec.rb
@@ -9,9 +9,10 @@ RSpec.describe ::Avm::Git::AutoCommitPath, git: true do
     {
       'app/models/mynamespace/the_class.rb' => 'Mynamespace::TheClass',
       'lib/ruby/lib/cliutils/eac_redmine_base0/activity.rb' => 'Cliutils::EacRedmineBase0::Activity'
-    }.each do |path, expected_class_name|
-      context "when path is \"#{path}\"" do
-        let(:instance) { described_class.new(git, "#{git}/#{path}") }
+    }.each do |relative_path, expected_class_name|
+      context "when path is \"#{relative_path}\"" do
+        let(:path) { git.root_path.join(relative_path) }
+        let(:instance) { described_class.new(git, path) }
 
         it { expect(instance.class_name).to eq(expected_class_name) }
       end
