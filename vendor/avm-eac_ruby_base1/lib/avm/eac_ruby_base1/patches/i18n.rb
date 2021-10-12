b8b0fa8db9a90e619b568da4f9b89775b985985c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-12 12:20:56 -0300

    avm/eac_ruby_base1/patches/i18n.rb.

---
+++ vendor/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/patches/i18n.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'i18n'
+
+::I18n.load_path += __dir__.to_pathname.expand_path.parent_n(4).join('locale').glob('*.yaml')
+                           .map(&:to_path)
