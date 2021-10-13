5b33e427907f2265685600690ae9e2b5851b8a49
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-13 13:09:04 -0300

    Object.translate: remove.

--- lib/avm/patches/object/i18n.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/patches/class/i18n'
-
-class Object
-  def translate(entry_suffix, values = {})
-    self.class.translate(entry_suffix, values)
-  end
-end
