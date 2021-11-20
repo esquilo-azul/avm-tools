4b065999a3aef1f8676a49444267ae326ec15497
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-11-20 19:36:03 -0300

    Avm::Sources::Base::Configuration.configuration.

--- sub/avm/lib/avm/sources/base/configuration.rb
+++ sub/avm/lib/avm/sources/base/configuration.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/yaml'
 
 module Avm
   module Sources
     class Base
       module Configuration
         private
+
+        # @return [Hash]
+        def configuration_uncached
+          ::Avm::Sources::Configuration::FILENAMES.each do |filename|
+            file_path = path.join(filename)
+            return ::EacRubyUtils::Yaml.load_file(file_path).with_indifferent_access if
+            file_path.exist?
+          end
+          {}
+        end
 
         # @return [Avm::Sources::Configuration]
         def old_configuration_uncached
