9e54e87004f6b0c519ab715273864c9cb7a55855
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-11-20 19:42:33 -0300

    Avm::Sources::Base::Configuration.read_configuration_as_shell_words.

--- sub/avm/lib/avm/sources/base/configuration.rb
+++ sub/avm/lib/avm/sources/base/configuration.rb
@@ -2,11 +2,19 @@
 
 require 'eac_ruby_utils/core_ext'
 require 'eac_ruby_utils/yaml'
+require 'shellwords'
 
 module Avm
   module Sources
     class Base
       module Configuration
+        # @return [Array<String>, nil]
+        def read_configuration_as_shell_words(key)
+          configuration[key].if_present do |v|
+            v.is_a?(::Enumerable) ? v.map(&:to_s) : ::Shellwords.split(v.to_s)
+          end
+        end
+
         private
 
         # @return [Hash]
