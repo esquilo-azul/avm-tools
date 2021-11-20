261c5d7622e0dc181054244133e887d715cba60e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-11-20 19:47:47 -0300

    Avm::SourceStereotypes::Base.tester.

--- sub/avm/lib/avm/source_stereotypes/base.rb
+++ sub/avm/lib/avm/source_stereotypes/base.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require 'avm/source_stereotypes/tester'
 require 'eac_ruby_utils/core_ext'
+require 'eac_ruby_utils/envs'
 
 module Avm
   module SourceStereotypes
@@ -11,6 +13,16 @@ module Avm
 
       def name
         self.class.name
+      end
+
+      # @return [Avm::SourceStereotypes::Tester]
+      def tester
+        tester_class.new(self)
+      end
+
+      # @return [Class<Avm::SourceStereotypes::Tester>
+      def tester_class
+        Avm::SourceStereotypes::Tester
       end
 
       def to_s
