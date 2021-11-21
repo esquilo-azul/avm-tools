ebcff486d273907cb41ae84d1d8aa83c297eb603
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-11-20 23:47:56 -0300

    Avm::EacRubyBase1::SourceStereotypes::Base.tester.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/source_stereotypes/base.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/source_stereotypes/base.rb
@@ -2,6 +2,7 @@
 
 require 'avm/source_stereotypes/base'
 require 'avm/eac_ruby_base1/source_stereotypes/update'
+require 'avm/eac_ruby_base1/source_stereotypes/tester'
 require 'eac_ruby_gems_utils/gem'
 require 'eac_ruby_utils/core_ext'
 
@@ -17,6 +18,11 @@ module Avm
 
         def valid?
           gemfile_path.exist? || gemspec_path.present?
+        end
+
+        # @return [Avm::EacRubyBase1::SourceStereotypes::Tester]
+        def tester_class
+          Avm::EacRubyBase1::SourceStereotypes::Tester
         end
 
         # @return [EacRubyGemsUtils::Gem]
