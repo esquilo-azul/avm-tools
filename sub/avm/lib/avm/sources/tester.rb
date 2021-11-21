fe71d475cd543721cd80c53ab133dfedc45e7434
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-11-21 14:16:01 -0300

    Avm::Sources::Tester: move behavior to "avm-eac_generic_base0":Avm::EacGenericBase0::Sources::Tester / make abstract.

--- sub/avm/lib/avm/sources/tester.rb
+++ sub/avm/lib/avm/sources/tester.rb
@@ -4,47 +4,20 @@ require 'eac_ruby_utils/core_ext'
 
 module Avm
   module Sources
-    module Tester
-      TEST_COMMAND_CONFIGURATION_KEY = :test_command
-
-      enable_simple_cache
+    class Tester
+      enable_abstract_methods
       common_constructor :source
 
-      # @return [EacRubyUtils::Envs::Command, nil]
-      def test_command
-        source.read_configuration_as_shell_words(TEST_COMMAND_CONFIGURATION_KEY)
-              .if_present do |args|
-          ::EacRubyUtils::Envs.local.command(args).chdir(source.path)
-        end
-      end
+      abstract_methods :result, :logs
 
-      private
-
-      def logs_uncached
-        ::EacRubyUtils::Fs::Logs.new.add(:stdout).add(:stderr)
+      # @return [EacRubyUtils::Fs::Logs]
+      def logs
+        raise_abstract_method __method__
       end
 
       # @return [Avm::Sources::Tests::Result]
-      def result_uncached
-        return ::Avm::Sources::Tests::Result::NONEXISTENT if test_command.blank?
-
-        if execute_command_and_log(test_command)
-          ::Avm::Sources::Tests::Result::SUCESSFUL
-        else
-          ::Avm::Sources::Tests::Result::FAILED
-        end
-      end
-
-      def run_test_command
-        execute_command_and_log(test_command)
-      end
-
-      # @return [true, false]
-      def execute_command_and_log(command)
-        r = command.execute
-        logs[:stdout].write(r[:stdout])
-        logs[:stderr].write(r[:stderr])
-        r[:exit_code].zero?
+      def result
+        raise_abstract_method __method__
       end
     end
   end
