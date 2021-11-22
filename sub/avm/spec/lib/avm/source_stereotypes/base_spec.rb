3a90df1d10d2d55c1a32c2fab51602ae25e0eb52
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-11-22 10:22:06 -0300

    Avm::SourceStereotypes::Base: remove.

--- sub/avm/spec/lib/avm/source_stereotypes/base_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/source_stereotypes/base'
-
-::RSpec.describe ::Avm::SourceStereotypes::Base do
-  include_examples 'in_avm_registry', :source_stereotypes
-end
