5a9c0d2a014a3a153a5fbce0a1fbdf35a19eefd5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-05 13:14:06 -0300

    Avm::EacRailsBase0::*: move from "avm-tools":Avm::EacRailsBase0::*.

---
+++ sub/avm-eac_rails_base0/lib/avm/eac_rails_base0/apache_path.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'avm/eac_webapp_base0/apache_path'
+require 'eac_ruby_utils/core_ext'
+
+module Avm
+  module EacRailsBase0
+    class ApachePath < ::Avm::EacWebappBase0::ApachePath
+      def document_root
+        ::File.join(super, 'public')
+      end
+
+      def extra_content
+        template.child('extra_content.conf').apply(instance)
+      end
+    end
+  end
+end
