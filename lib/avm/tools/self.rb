b67fd54b4362d5ed3e5b3c3a9850c6ea0d4ce040
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-11 23:38:42 -0300

    Avm::Tools::Self.build_config: remove unused.

--- lib/avm/tools/self.rb
+++ lib/avm/tools/self.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'eac_cli/old_configs_bridge'
 require 'eac_ruby_base0/application'
 require 'eac_ruby_utils/require_sub'
 require 'avm/instances/base'
@@ -13,14 +12,6 @@ module Avm
       class << self
         def application
           @application ||= ::EacRubyBase0::Application.new(root.to_path)
-        end
-
-        # @return [EacCli::OldConfigsBridge]
-        def build_config(path = nil)
-          ::EacCli::OldConfigsBridge.new(
-            application.name,
-            path.if_present({}) { |v| { storage_path: v } }
-          )
         end
 
         def instance
