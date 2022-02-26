f06215bf2823101f30c744b926c783c5a8244223
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-02-26 12:42:16 -0300

    Gem "eac_rest" (Local): add '0.3.4'.

---
+++ sub/eac_rest/lib/eac_rest/api.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'eac_rest/request'
+require 'eac_ruby_utils/core_ext'
+
+module EacRest
+  # Abstract methods
+  #   * self.issue_get_url_suffix(provider_issue_id)
+  class Api
+    require_sub __FILE__, include_modules: true
+    common_constructor :root_url, :username, :password, default: [nil, nil]
+
+    def request(service_url_suffix, headers = {}, &body_data_proc)
+      r = ::EacRest::Request.new(build_service_url(service_url_suffix),
+                                 body_data_proc)
+      headers.each { |name, value| r = r.header(name, value) }
+      r = r.autenticate(username, password) if username.present?
+      r
+    end
+
+    def request_json(service_url_suffix, headers = {}, &body_data_proc)
+      request(service_url_suffix, headers.merge('Accept' => 'application/json')) do |body_data|
+        r = ::JSON.parse(body_data)
+        r = body_data_proc.call(r) if body_data_proc
+        r
+      end
+    end
+
+    def build_service_url(suffix)
+      s = ::Addressable::URI.parse(suffix)
+      r = ::Addressable::URI.parse(root_url)
+      r.path += s.path
+      r.query_values = r.query_values(::Array).if_present([]) +
+                       s.query_values(::Array).if_present([])
+      r.to_s
+    end
+  end
+end
