c70d471ba5fb2f9c08f4695b3060e8f7c0e5a4f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-12 19:39:20 -0300

    Avm::Sources::Base::Parent.parent_by_search.

--- sub/avm/lib/avm/sources/base/parent.rb
+++ sub/avm/lib/avm/sources/base/parent.rb
@@ -8,12 +8,22 @@ module Avm
       module Parent
         # @return [Avm::Sources::Base]
         def parent
-          parent_by_option
+          parent_by_option || parent_by_search
         end
 
         # @return [Avm::Sources::Base]
         def parent_by_option
           options[OPTION_PARENT]
+        end
+
+        # @return [Avm::Sources::Base]
+        def parent_by_search
+          parent_path = path.parent
+          until parent_path.root?
+            ::Avm::Registry.sources.detect_optional(parent_path).if_present { |v| return v }
+            parent_path = parent_path.parent
+          end
+          nil
         end
       end
     end
