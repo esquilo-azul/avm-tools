3d9c16e29f0705a651e264bf3be8dfcec181d103
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-12 21:10:33 -0300

    Avm::Scms::Base.parent_scm.

--- sub/avm/lib/avm/scms/base.rb
+++ sub/avm/lib/avm/scms/base.rb
@@ -6,6 +6,7 @@ module Avm
   module Scms
     class Base
       enable_abstract_methods
+      enable_simple_cache
       abstract_methods :update, :valid?
       common_constructor :path do
         self.path = path.to_pathname
@@ -27,6 +28,18 @@ module Avm
 
       def to_s
         name
+      end
+
+      private
+
+      # @return [Avm::Scms::Base]
+      def parent_scm
+        parent_path = path.parent
+        until parent_path.root?
+          ::Avm::Registry.scms.detect_optional(parent_path).if_present { |v| return v }
+          parent_path = parent_path.parent
+        end
+        nil
       end
     end
   end
