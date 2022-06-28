f4c348f67777524ae082b76796203a8eadee828b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-28 19:13:51 -0300

    Avm::EacRubyBase1::Sources::Base::Bundler.bundle.

--- sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/sources/base/bundler.rb
+++ sub/avm-eac_ruby_base1/lib/avm/eac_ruby_base1/sources/base/bundler.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'avm/eac_generic_base0/sources/base'
+require 'avm/eac_ruby_base1/sources/base/bundle_command'
 require 'eac_ruby_utils/core_ext'
 
 module Avm
@@ -10,6 +11,12 @@ module Avm
         module Bundler
           CONFIGURED_GEMFILE_PATH_ENTRY_KEY = 'ruby.gemfile_path'
           DEFAULT_GEMFILE_PATH = 'Gemfile'
+
+          # @return [Avm::EacRubyBase1::Sources::Base::BundleCommand]
+          def bundle(*args)
+            ::Avm::EacRubyBase1::Sources::Base::BundleCommand.new(self, %w[bundle] + args)
+              .envvar_gemfile
+          end
 
           # @return [String]
           def configured_gemfile_path
