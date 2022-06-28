4c75fd17b36609531419b63797de6cb393d0430b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-28 13:46:31 -0300

    Avm::Ruby::Bundler::IncompatibleParser: move to "avm-eac_ruby_base1": Avm::EacRubyBase1::Bundler::IncompatibleParser.

--- spec/lib/avm/ruby/bundler/incompatible_parser_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'avm/ruby/bundler/incompatible_parser'
-
-RSpec.describe ::Avm::Ruby::Bundler::IncompatibleParser do
-  include_examples 'source_target_fixtures', __FILE__
-end
